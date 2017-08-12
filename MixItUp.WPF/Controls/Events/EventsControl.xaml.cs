f6e1ed0e0098c3683d073d758b05f3ffb01ebd78
Matthew Olivo <saviorxtanren@gmail.com>
2017-08-12 12:08:37 -0700

    - Initial Events control UI

--- MixItUp.WPF/Controls/Events/EventsControl.xaml.cs
+++ MixItUp.WPF/Controls/Events/EventsControl.xaml.cs
@@ -1,13 +1,47 @@
-﻿namespace MixItUp.WPF.Controls.Events
+﻿using Mixer.Base.Clients;
+using MixItUp.Base;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System;
+using System.Threading.Tasks;
+using Mixer.Base.Util;
+
+namespace MixItUp.WPF.Controls.Events
 {
     /// <summary>
     /// Interaction logic for EventsControl.xaml
     /// </summary>
     public partial class EventsControl : MainControlBase
     {
+        private ObservableCollection<string> eventTypes = new ObservableCollection<string>();
+
+        private List<ConstellationEventType> subscribedEvents = new List<ConstellationEventType>();
+
         public EventsControl()
         {
             InitializeComponent();
+        }
+
+        protected override async Task InitializeInternal()
+        {
+            if (await MixerAPIHandler.InitializeConstellationClient())
+            {
+                if (this.subscribedEvents.Count > 0)
+                {
+                    await MixerAPIHandler.ConstellationClient.LiveSubscribe(this.subscribedEvents);
+                }
+
+
+                foreach (string name in EnumHelper.GetEnumNames<ConstellationEventTypeEnum>())
+                {
+                    eventTypes.Add(name);
+                }
+            }
+        }
+
+        private void ExportDataButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
         }
     }
 }
