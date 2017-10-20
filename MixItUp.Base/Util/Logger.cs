2beb2ce64957bb93199c189a34c77dd223cb3405
Matthew Olivo <saviorxtanren@gmail.com>
2017-10-19 23:15:01 -0700

    - Adding stack trace capturing for exceptions

--- MixItUp.Base/Util/Logger.cs
+++ MixItUp.Base/Util/Logger.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 
@@ -32,6 +33,13 @@ namespace MixItUp.Base.Util
             catch (Exception) { }
         }
 
-        public static void Log(Exception ex) { Logger.Log(ex.ToString()); }
+        public static void Log(Exception ex)
+        {
+            Logger.Log("Exception caught at:");
+            StackTrace stack = new StackTrace();
+            Logger.Log(stack.ToString());
+
+            Logger.Log(ex.ToString());
+        }
     }
 }
