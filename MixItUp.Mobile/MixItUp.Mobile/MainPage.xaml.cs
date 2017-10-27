70602e9a970a0e8ea6b3c8ab3b1c1c6956a67525
Matthew Olivo <saviorxtanren@gmail.com>
2017-10-26 17:02:14 -0700

    - Adding test for phone auth

--- MixItUp.Mobile/MixItUp.Mobile/MainPage.xaml.cs
+++ MixItUp.Mobile/MixItUp.Mobile/MainPage.xaml.cs
@@ -1,17 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Mixer.Base;
+using System;
 using Xamarin.Forms;
 
 namespace MixItUp.Mobile
 {
-	public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage
 	{
 		public MainPage()
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void StreamerButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void ModeratorButton_Clicked(object sender, EventArgs e)
+        {
+            this.LoginWebView.IsVisible = true;
+            this.LoginButtonsStack.IsVisible = false;
+
+            this.LoginWebView.Source = await MixerConnection.GetAuthorizationCodeURLForOAuthBrowser(ChannelSession.ClientID, ChannelSession.ModeratorScopes, MixerConnection.OAUTH_LOCALHOST_URL);
+            //await ChannelSession.ConnectUser(ChannelSession.ModeratorScopes, "SaviorXTanren");
+        }
+
+        private void RemoteButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
