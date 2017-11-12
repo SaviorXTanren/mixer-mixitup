ec234db6998033771e3106a9d7bcb6200e222dfc
Matthew Olivo <saviorxtanren@gmail.com>
2017-11-12 14:25:37 -0800

    - Initial new user wizard

---
+++ MixItUp.WPF/Windows/Wizard/NewUserWizardWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+namespace MixItUp.WPF.Windows.Wizard
+{
+    /// <summary>
+    /// Interaction logic for NewUserWizardWindow.xaml
+    /// </summary>
+    public partial class NewUserWizardWindow : LoadingWindowBase
+    {
+        public NewUserWizardWindow()
+        {
+            InitializeComponent();
+
+            this.Initialize(this.StatusBar);
+        }
+
+        protected override Task OnLoaded()
+        {
+            return base.OnLoaded();
+        }
+    }
+}
