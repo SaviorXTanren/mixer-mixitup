41a108aceaebcba30649686564dc5587e22dfcc1
Matthew Olivo <saviorxtanren@gmail.com>
2018-01-28 01:08:25 -0800

    - Create custom "new update" UI that looks more aligned with Mix It Up design

---
+++ MixItUp.WPF/UpdateWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using MixItUp.WPF.Windows;
+using System.Windows;
+using System.Threading.Tasks;
+using AutoUpdaterDotNET;
+using System;
+
+namespace MixItUp.WPF
+{
+    /// <summary>
+    /// Interaction logic for UpdateWindow.xaml
+    /// </summary>
+    public partial class UpdateWindow : LoadingWindowBase
+    {
+        private UpdateInfoEventArgs updateArgs;
+
+        public UpdateWindow(UpdateInfoEventArgs updateArgs)
+        {
+            this.updateArgs = updateArgs;
+
+            InitializeComponent();
+
+            this.Initialize(this.StatusBar);
+        }
+
+        protected override Task OnLoaded()
+        {
+            this.NewVersionTextBlock.Text = updateArgs.CurrentVersion.ToString();
+            this.UpdateChangelogWebBrowser.Source = new Uri(updateArgs.ChangelogURL);
+
+            return base.OnLoaded();
+        }
+
+        private void DownloadUpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (AutoUpdater.DownloadUpdate())
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void SkipUpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
