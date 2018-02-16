64008dfab057f173f7d09ce16b587a4ce0d298a9
Matthew Olivo <saviorxtanren@gmail.com>
2018-02-15 19:31:32 -0800

    - Adding initial pipe-work for Localization support

---
+++ MixItUp.Base/Localization/LocalizationHandler.cs
@@ -0,0 +1,49 @@
+﻿using MixItUp.Base.Util;
+using System;
+using System.Resources;
+
+namespace MixItUp.Base.Localization
+{
+    public static class LocalizationHandler
+    {
+        private static ResourceManager ResourceManager = null;
+
+        public static void SetCurrentLanguage(string languageName)
+        {
+            switch (languageName)
+            {
+                case "jp":
+                    LocalizationHandler.ResourceManager = MixItUp.Base.Localization.Japanese.ResourceManager;
+                    break;
+                case "sp":
+                    LocalizationHandler.ResourceManager = MixItUp.Base.Localization.Spanish.ResourceManager;
+                    break;
+                case "en":
+                default:
+                    LocalizationHandler.ResourceManager = MixItUp.Base.Localization.English.ResourceManager;
+                    break;
+            }
+        }
+
+        public static string GetLocalizationString(string key)
+        {
+            return LocalizationHandler.GetLocalizationString(LocalizationHandler.ResourceManager, key);
+        }
+
+        public static string GetLocalizationString(ResourceManager resourceManager, string key)
+        {
+            string value = null;
+            try
+            {
+                value = resourceManager.GetString(key);
+            }
+            catch (Exception ex) { Logger.Log(ex); }
+
+            if (string.IsNullOrEmpty(value))
+            {
+                return LocalizationHandler.GetLocalizationString(MixItUp.Base.Localization.English.ResourceManager, key);
+            }
+            return value;
+        }
+    }
+}
---
+++ MixItUp.WPF/Util/LocalizationConverter.cs
@@ -0,0 +1,25 @@
+﻿using MixItUp.Base.Localization;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MixItUp.WPF.Util
+{
+    public class LocalizationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var key = parameter as string;
+            if (!string.IsNullOrEmpty(key))
+            {
+                return LocalizationHandler.GetLocalizationString(key);
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
