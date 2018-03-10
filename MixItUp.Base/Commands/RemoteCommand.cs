0da465f98e5e9515d66880cfe120ee899609827a
Matthew Olivo <saviorxtanren@gmail.com>
2018-03-09 19:58:04 -0800

    - Adding export for RemoteCommand

--- MixItUp.Base/Commands/RemoteCommand.cs
+++ MixItUp.Base/Commands/RemoteCommand.cs
@@ -1,4 +1,5 @@
-﻿using MixItUp.Base.ViewModel.User;
+﻿using MixItUp.Base.Model.Remote;
+using MixItUp.Base.ViewModel.User;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -40,6 +41,15 @@ namespace MixItUp.Base.Commands
         }
 
         public override bool IsEditable { get { return (this.ReferenceCommand == null); } }
+
+        public RemoteBoardItemModelBase GetRemoteBoardItem()
+        {
+            return new RemoteBoardButtonModel()
+            {
+                ID = this.ID,
+                Name = this.Name
+            };
+        }
 
         protected override SemaphoreSlim AsyncSemaphore { get { return RemoteCommand.remoteCommandPerformSemaphore; } }
 
