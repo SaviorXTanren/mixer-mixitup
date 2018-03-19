6a9d9636186f7b17d48482d67702b008a49b0b3d
Matthew Olivo <saviorxtanren@gmail.com>
2018-03-18 22:41:09 -0700

    - Disabling GameWisp & Spotify services temporarily

--- MixItUp.WPF/Controls/MainControls/ServicesControl.xaml.cs
+++ MixItUp.WPF/Controls/MainControls/ServicesControl.xaml.cs
@@ -28,8 +28,8 @@ namespace MixItUp.WPF.Controls.MainControls
             this.services.Add(new ServicesGroupBoxControl(this.Window, new XSplitServiceControl()));
             this.services.Add(new ServicesGroupBoxControl(this.Window, new TwitterServiceControl()));
             this.services.Add(new ServicesGroupBoxControl(this.Window, new StreamlabsServiceControl()));
-            this.services.Add(new ServicesGroupBoxControl(this.Window, new GameWispServiceControl()));
-            this.services.Add(new ServicesGroupBoxControl(this.Window, new SpotifyServiceControl()));
+            //this.services.Add(new ServicesGroupBoxControl(this.Window, new GameWispServiceControl()));
+            //this.services.Add(new ServicesGroupBoxControl(this.Window, new SpotifyServiceControl()));
             this.services.Add(new ServicesGroupBoxControl(this.Window, new DeveloperAPIServiceControl()));
 
             await base.InitializeInternal();
