81d2b8d5b026f485b70603a006015cb74228901b
Matthew Olivo <saviorxtanren@gmail.com>
2018-03-20 01:08:45 -0700

    - Create Mix It Up installer project

---
+++ MixItUp.Installer/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+
+namespace MixItUp.Installer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        }
+
+        private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            AssemblyName assemblyName = new AssemblyName(args.Name);
+
+            var path = assemblyName.Name + ".dll";
+            if (assemblyName.CultureInfo.Equals(CultureInfo.InvariantCulture) == false) path = String.Format(@"{0}\{1}", assemblyName.CultureInfo, path);
+
+            using (Stream stream = executingAssembly.GetManifestResourceStream(path))
+            {
+                if (stream == null) return null;
+
+                var assemblyRawBytes = new byte[stream.Length];
+                stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
+                return Assembly.Load(assemblyRawBytes);
+            }
+        }
+    }
+}
---
+++ MixItUp.Installer/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using IWshRuntimeLibrary;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MixItUp.Installer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                try
+                {
+                    string autoUpdaterFile = null;
+                    using (WebClient client = new WebClient())
+                    {
+                        autoUpdaterFile = client.DownloadString(new System.Uri("https://updates.mixitupapp.com/AutoUpdater.xml"));
+                    }
+
+                    if (!string.IsNullOrEmpty(autoUpdaterFile))
+                    {
+                        string updateURL = autoUpdaterFile;
+                        updateURL = updateURL.Substring(updateURL.IndexOf("<url>"));
+                        updateURL = updateURL.Replace("<url>", "");
+                        updateURL = updateURL.Substring(0, updateURL.IndexOf("</url>"));
+
+                        string updateFilePath = Path.Combine(Path.GetTempPath(), "MixItUp.zip");
+                        using (WebClient client = new WebClient())
+                        {
+                            client.DownloadFile(updateURL, updateFilePath);
+                        }
+
+                        if (System.IO.File.Exists(updateFilePath))
+                        {
+                            string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "MixItUp");
+
+                            if (Directory.Exists(folderPath))
+                            {
+                                Directory.Delete(folderPath, recursive: true);
+                            }
+
+                            Directory.CreateDirectory(folderPath);
+                            if (Directory.Exists(folderPath))
+                            {
+                                ZipFile.ExtractToDirectory(updateFilePath, folderPath);
+
+                                string applicationPath = Path.Combine(folderPath, "MixItUp.exe");
+                                if (System.IO.File.Exists(applicationPath))
+                                {
+                                    string shortcutPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), "Mix It Up");
+
+                                    if (Directory.Exists(shortcutPath))
+                                    {
+                                        Directory.Delete(shortcutPath, recursive: true);
+                                    }
+
+                                    Directory.CreateDirectory(shortcutPath);
+                                    if (Directory.Exists(shortcutPath))
+                                    {
+                                        WshShell wsh = new WshShell();
+                                        IWshRuntimeLibrary.IWshShortcut shortcut = wsh.CreateShortcut(Path.Combine(shortcutPath, "Mix It Up.lnk")) as IWshRuntimeLibrary.IWshShortcut;
+                                        shortcut.Arguments = "";
+                                        shortcut.TargetPath = applicationPath;
+                                        shortcut.WindowStyle = 1;
+                                        shortcut.Description = "Mix It Up";
+                                        shortcut.WorkingDirectory = folderPath;
+                                        shortcut.IconLocation = applicationPath;
+                                        shortcut.Save();
+
+                                        Process.Start(applicationPath);
+                                        this.Dispatcher.Invoke(() =>
+                                        {
+                                            this.Close();
+                                        });
+                                        return;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex) { Console.WriteLine(ex); }
+
+                this.ShowError("We were unable to download the latest Mix It Up version. Please contact support@mixitupapp.com for assistance.");
+            });
+        }
+
+        private void ShowError(string message)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                this.InstallProgressBar.Visibility = Visibility.Collapsed;
+                this.InstallingTextBlock1.Visibility = Visibility.Collapsed;
+                this.InstallingTextBlock2.Visibility = Visibility.Collapsed;
+
+                this.ErrorTextBlock.Visibility = Visibility.Visible;
+                this.ErrorTextBlock.Text = message;
+            });
+        }
+    }
+}
