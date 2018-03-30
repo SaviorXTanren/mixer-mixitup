c51a37a4a11119c95d3a0f358569765c17226e99
Matthew Olivo <saviorxtanren@gmail.com>
2018-03-30 14:06:45 -0700

    - Updating text for command unlocking and adding tooltip text

--- MixItUp.WPF/Controls/Command/UnlockedCommandControl.xaml.cs
+++ MixItUp.WPF/Controls/Command/UnlockedCommandControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
 
 namespace MixItUp.WPF.Controls.Command
 {
@@ -7,15 +8,29 @@ namespace MixItUp.WPF.Controls.Command
     /// </summary>
     public partial class UnlockedCommandControl : UserControl
     {
+        private static readonly string UnlockedGridTooltip =
+            "Mix It Up has build in Command locking functionality which ensures only" + Environment.NewLine +
+            "1 command type (Chat, Interactive, etc) can run at the same time and" + Environment.NewLine +
+            "ensures that each command finishes in the order it was run in." + Environment.NewLine + Environment.NewLine +
+            "This option will allow you to disable locking on this command, but be aware" + Environment.NewLine +
+            "that this could cause some unforeseen issues so please use with caution.";
+
         public UnlockedCommandControl()
         {
             InitializeComponent();
+
+            this.Loaded += UnlockedCommandControl_Loaded;
+        }
+
+        private void UnlockedCommandControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.UnlockedGrid.ToolTip = UnlockedGridTooltip;
         }
 
         public bool Unlocked
         {
-            get { return this.DontBlockCommandToggleButton.IsChecked.GetValueOrDefault(); }
-            set { this.DontBlockCommandToggleButton.IsChecked = value; }
+            get { return this.UnlockCommandToggleButton.IsChecked.GetValueOrDefault(); }
+            set { this.UnlockCommandToggleButton.IsChecked = value; }
         }
     }
 }
