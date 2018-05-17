b582a6963f91733b4488bb3a91eb45498dab9f8a
Matthew Olivo <saviorxtanren@gmail.com>
2018-05-16 21:06:23 -0700

    - Add Mixer status alert icon to Login UI

--- MixItUp.WPF/Controls/MainControls/MainMenuControl.xaml.cs
+++ MixItUp.WPF/Controls/MainControls/MainMenuControl.xaml.cs
@@ -81,10 +81,6 @@ namespace MixItUp.WPF.Controls.MainControls
 
         protected override async Task InitializeInternal()
         {
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-            Task.Run(async () => await this.CheckMixerStatus());
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-
             this.MenuItemsListBox.ItemsSource = this.menuItems;
 
             await this.MainSettings.Initialize(this.Window);
@@ -141,42 +137,6 @@ namespace MixItUp.WPF.Controls.MainControls
                 Process.Start(item.Link);
             }
             this.MenuToggleButton.IsChecked = false;
-        }
-
-        private void MixerStatusAlertButton_Click(object sender, RoutedEventArgs e) { Process.Start("https://status.mixer.com/"); }
-
-        private async Task CheckMixerStatus()
-        {
-            while (true)
-            {
-                try
-                {
-                    IEnumerable<MixerIncident> incidents = await ChannelSession.Services.MixerStatus.GetCurrentIncidents();
-
-                    await this.Dispatcher.InvokeAsync(() =>
-                    {
-                        if (incidents != null && incidents.Count() > 0)
-                        {
-                            this.MixerStatusAlertButton.Visibility = Visibility.Visible;
-                            StringBuilder tooltip = new StringBuilder();
-                            tooltip.AppendLine("Mixer Status Alert:");
-                            foreach (MixerIncident incident in incidents)
-                            {
-                                tooltip.AppendLine();
-                                tooltip.AppendLine(string.Format("{0} - Last Updated: {1}", incident.Title, incident.LastUpdate.ToString("G")));
-                            }
-                            this.MixerStatusAlertButton.ToolTip = tooltip.ToString();
-                        }
-                        else
-                        {
-                            this.MixerStatusAlertButton.Visibility = Visibility.Collapsed;
-                        }
-                    });
-
-                    await Task.Delay(60000);
-                }
-                catch (Exception ex) { MixItUp.Base.Util.Logger.Log(ex); }
-            }
         }
 
         private async void GlobalEvents_OnServiceDisconnect(object sender, string serviceName)
---
+++ MixItUp.WPF/Controls/MixerStatusAlertControl.xaml.cs
@@ -0,0 +1,69 @@
+﻿using MixItUp.Base;
+using MixItUp.Base.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MixItUp.WPF.Controls
+{
+    /// <summary>
+    /// Interaction logic for MixerStatusAlertControl.xaml
+    /// </summary>
+    public partial class MixerStatusAlertControl : UserControl
+    {
+        public MixerStatusAlertControl()
+        {
+            InitializeComponent();
+
+            this.Loaded += MixerStatusAlertControl_Loaded;
+        }
+
+        private void MixerStatusAlertControl_Loaded(object sender, RoutedEventArgs e)
+        {
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            Task.Run(async () => await this.CheckMixerStatus());
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+        }
+
+        private void MixerStatusAlertButton_Click(object sender, RoutedEventArgs e) { Process.Start("https://status.mixer.com/"); }
+
+        private async Task CheckMixerStatus()
+        {
+            while (true)
+            {
+                try
+                {
+                    IEnumerable<MixerIncident> incidents = await ChannelSession.Services.MixerStatus.GetCurrentIncidents();
+
+                    await this.Dispatcher.InvokeAsync(() =>
+                    {
+                        if (incidents != null && incidents.Count() > 0)
+                        {
+                            this.MixerStatusAlertButton.Visibility = Visibility.Visible;
+                            StringBuilder tooltip = new StringBuilder();
+                            tooltip.AppendLine("Mixer Status Alert:");
+                            foreach (MixerIncident incident in incidents)
+                            {
+                                tooltip.AppendLine();
+                                tooltip.AppendLine(string.Format("{0} - Last Updated: {1}", incident.Title, incident.LastUpdate.ToString("G")));
+                            }
+                            this.MixerStatusAlertButton.ToolTip = tooltip.ToString();
+                        }
+                        else
+                        {
+                            this.MixerStatusAlertButton.Visibility = Visibility.Hidden;
+                        }
+                    });
+
+                    await Task.Delay(60000);
+                }
+                catch (Exception ex) { MixItUp.Base.Util.Logger.Log(ex); }
+            }
+        }
+    }
+}
