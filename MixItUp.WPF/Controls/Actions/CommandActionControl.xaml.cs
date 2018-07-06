5d16a268b6631ef317753909f4d1c3ba07fb31c4
Shane DeSeranno <tyren@syntheticstream.com>
2018-07-05 17:33:11 -0700

    Remove unnecessary constant

--- MixItUp.WPF/Controls/Actions/CommandActionControl.xaml.cs
+++ MixItUp.WPF/Controls/Actions/CommandActionControl.xaml.cs
@@ -13,8 +13,6 @@ namespace MixItUp.WPF.Controls.Actions
 {
     public partial class CommandActionControl : ActionControlBase
     {
-        private static readonly List<int> sampleFontSize = new List<int>() { 12, 24, 36, 48, 60, 72, 84, 96, 108, 120 };
-
         private CommandAction command;
 
         public CommandActionControl(ActionContainerControl containerControl) : base(containerControl) { InitializeComponent(); }
