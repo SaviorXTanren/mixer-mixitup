2dceca05f14b3b5ac3fadc5a3fc6c3e25d7e5f37
Shane DeSeranno <tyren@syntheticstream.com>
2018-07-12 21:50:24 -0700

    Add basic telemetry

---
+++ MixItUp.Desktop/Services/DesktopTelemetryService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.ApplicationInsights;
+using MixItUp.Base;
+using MixItUp.Base.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixItUp.Desktop.Services
+{
+    public class DesktopTelemetryService : ITelemetryService
+    {
+        private TelemetryClient telemetryClient = new TelemetryClient();
+
+        public DesktopTelemetryService()
+        {
+            this.telemetryClient.Context.Cloud.RoleInstance = "MixItUpApp";
+            this.telemetryClient.Context.Cloud.RoleName = "MixItUpApp";
+            this.telemetryClient.Context.Session.Id = Guid.NewGuid().ToString();
+            this.telemetryClient.Context.Device.OperatingSystem = Environment.OSVersion.ToString();
+        }
+
+        public void TrackException(Exception ex)
+        {
+            this.telemetryClient.TrackException(ex);
+        }
+
+        public void TrackPageView(string pageName)
+        {
+            this.telemetryClient.TrackPageView(pageName);
+        }
+
+        public void Start()
+        {
+            this.telemetryClient.InstrumentationKey = ChannelSession.SecretManager.GetSecret("ApplicationInsightsKey");
+        }
+
+        public void End()
+        {
+            this.telemetryClient.Flush();
+            Task.Delay(1000); // Allow time to flush
+        }
+    }
+}
