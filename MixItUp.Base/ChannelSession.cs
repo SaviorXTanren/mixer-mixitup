1891e9e17281e4e99d81812211aec79efbaff8ff
Shane DeSeranno <tyren@syntheticstream.com>
2018-08-29 14:25:53 -0700

    Do not check the userId equals channel userId when logging in as a mod

--- MixItUp.Base/ChannelSession.cs
+++ MixItUp.Base/ChannelSession.cs
@@ -385,7 +385,7 @@ namespace MixItUp.Base
                     }
                     await ChannelSession.Services.Settings.Initialize(ChannelSession.Settings);
 
-                    if (ChannelSession.Settings.Channel != null && ChannelSession.User.id != ChannelSession.Settings.Channel.userId)
+                    if (string.IsNullOrEmpty(channelName) && ChannelSession.Settings.Channel != null && ChannelSession.User.id != ChannelSession.Settings.Channel.userId)
                     {
                         GlobalEvents.ShowMessageBox("The account you are logged in as on Mixer does not match the account for this settings. Please log in as the correct account on Mixer.");
                         return false;
