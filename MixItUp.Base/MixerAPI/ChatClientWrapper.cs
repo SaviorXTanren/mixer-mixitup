59a0b6e93d6b3d40bb0811a7afe0512408d5cf71
Matthew Olivo <saviorxtanren@gmail.com>
2018-11-11 21:28:07 -0800

    - Adding extra channel background check for chat users

--- MixItUp.Base/MixerAPI/ChatClientWrapper.cs
+++ MixItUp.Base/MixerAPI/ChatClientWrapper.cs
@@ -306,6 +306,10 @@ namespace MixItUp.Base.MixerAPI
                         Task.Run(async () => { await this.TimerCommandsBackground(); }, this.backgroundThreadCancellationTokenSource.Token);
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                        Task.Run(async () => { await this.ChatterRefreshBackground(); }, this.backgroundThreadCancellationTokenSource.Token);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
                         return true;
                     }
                     return false;
@@ -603,6 +607,22 @@ namespace MixItUp.Base.MixerAPI
 
                     timerCommandIndex++;
                 }
+            });
+        }
+
+        private async Task ChatterRefreshBackground()
+        {
+            await BackgroundTaskWrapper.RunBackgroundTask(this.backgroundThreadCancellationTokenSource, async (tokenSource) =>
+            {
+                tokenSource.Token.ThrowIfCancellationRequested();
+
+                await ChannelSession.RefreshChannel();
+                await Task.Delay(180000, tokenSource.Token);
+
+                tokenSource.Token.ThrowIfCancellationRequested();
+
+                IEnumerable<ChatUserModel> chatters = await ChannelSession.Connection.GetChatUsers(ChannelSession.Channel, int.MaxValue);
+                await ChannelSession.ActiveUsers.AddOrUpdateUsers(chatters);
             });
         }
 
