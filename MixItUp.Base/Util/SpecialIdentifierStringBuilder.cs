269b23bc75d83f491a0ad74389a288123e3ad1ef
Matthew Olivo <saviorxtanren@gmail.com>
2018-11-28 10:48:47 -0800

    - Adding ordering of Currency/Rank special identifiers to hopefully prevent incorrect assignment when doing replacement

--- MixItUp.Base/Util/SpecialIdentifierStringBuilder.cs
+++ MixItUp.Base/Util/SpecialIdentifierStringBuilder.cs
@@ -610,7 +610,7 @@ namespace MixItUp.Base.Util
                 {
                     UserDataViewModel userData = ChannelSession.Settings.UserData[user.ID];
 
-                    foreach (UserCurrencyViewModel currency in ChannelSession.Settings.Currencies.Values)
+                    foreach (UserCurrencyViewModel currency in ChannelSession.Settings.Currencies.Values.OrderByDescending(c => c.Name))
                     {
                         UserCurrencyDataViewModel currencyData = userData.GetCurrency(currency);
                         UserRankViewModel rank = currencyData.GetRank();
