a41a4eaa8e1287d7398da25deba2725f8ea62583
Matthew Olivo <saviorxtanren@gmail.com>
2019-01-01 20:31:20 -0800

    - Completing Inventory system

---
+++ APIs/MixItUp.API/Models/AdjustInventory.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace MixItUp.API.Models
+{
+    [DataContract]
+    public class AdjustInventory
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public int Amount { get; set; }
+    }
+}
---
+++ MixItUp.Desktop/Services/DeveloperAPI/InventoryController.cs
@@ -0,0 +1,59 @@
+﻿using MixItUp.Base;
+using MixItUp.Base.ViewModel.User;
+using MixItUp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace MixItUp.Desktop.Services.DeveloperAPI
+{
+    [RoutePrefix("api/inventory")]
+    public class InventoryController : ApiController
+    {
+        [Route]
+        [HttpGet]
+        public IEnumerable<Inventory> Get()
+        {
+            List<Inventory> list = new List<Inventory>();
+            foreach (var inventory in ChannelSession.Settings.Inventories.Values)
+            {
+                list.Add(InventoryFromUserInventoryViewModel(inventory));
+            }
+            return list;
+        }
+
+        [Route("{inventoryID:guid}")]
+        [HttpGet]
+        public Inventory Get(Guid inventoryID)
+        {
+            if (!ChannelSession.Settings.Inventories.ContainsKey(inventoryID))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return InventoryFromUserInventoryViewModel(ChannelSession.Settings.Inventories[inventoryID]);
+        }
+
+        public static InventoryAmount InventoryAmountFromUserInventoryViewModel(UserInventoryViewModel inventory, UserInventoryDataViewModel inventoryData)
+        {
+            return new InventoryAmount
+            {
+                ID = inventory.ID,
+                Name = inventory.Name,
+                Items = inventoryData.Amounts.Select(kvp => new InventoryItemAmount() { Name = kvp.Key, Amount = kvp.Value }).ToList(),
+            };
+        }
+
+        public static Inventory InventoryFromUserInventoryViewModel(UserInventoryViewModel inventory)
+        {
+            return new Inventory
+            {
+                ID = inventory.ID,
+                Name = inventory.Name,
+                Items = inventory.Items.Keys.Select(i => new InventoryItem() { Name = i }).ToList(),
+            };
+        }
+    }
+}
