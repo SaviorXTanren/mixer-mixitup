a909d71c341c263ddd5fb3d8cbd6684d232adad0
Shane DeSeranno <tyren@syntheticstream.com>
2019-01-12 16:39:12 -0800

    Wire up the Mix Play Broadcast event and expose via the Developer APIs.

--- APIs/MixItUp.API/Models/SendChatWhisper.cs
+++ APIs/MixItUp.API/Models/SendChatWhisper.cs
@@ -10,6 +10,7 @@ namespace MixItUp.API.Models
         [DataMember]
         public string Message { get; set; }
 
+        [Required]
         [DataMember]
         public string UserName { get; set; }
 
