a48da7e98e0811ea00f0040f50514bfefb26c0ee
Shane DeSeranno <tyren@syntheticstream.com>
2019-01-28 12:36:08 -0800

    Add better error handling around stream deck detection

--- MixItUp.WPF/Controls/Services/StreamDeckServiceControl.xaml.cs
+++ MixItUp.WPF/Controls/Services/StreamDeckServiceControl.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Win32;
+using Mixer.Base.Util;
 using MixItUp.Base;
 using MixItUp.WPF.Util;
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
@@ -31,32 +33,36 @@ namespace MixItUp.WPF.Controls.Services
         {
             await this.groupBoxControl.window.RunAsyncOperation(async () =>
             {
-                string installDir = GetStreamDeckInstallationDirectory();
-                if (string.IsNullOrEmpty(installDir))
-                {
-                    await MessageBoxHelper.ShowMessageDialog("Stream Deck doesn't appear to be installed correctly.");
-                    return;
-                }
-
-                string streamDeckExe = Path.Combine(installDir, "StreamDeck.exe");
-                if (!File.Exists(streamDeckExe))
-                {
-                    await MessageBoxHelper.ShowMessageDialog("Could not find the Stream Deck application in the installation directory.");
-                    return;
-                }
-
-                FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(streamDeckExe);
-                if (versionInfo.FileMajorPart < 4)
-                {
-                    await MessageBoxHelper.ShowMessageDialog("Stream Deck is installed, but is not v4. Please update Stream Deck to v4.");
-                    return;
-                }
-
                 if (!ChannelSession.Settings.EnableDeveloperAPI)
                 {
                     await MessageBoxHelper.ShowMessageDialog("Please enable the Developer APIs before installing the Stream Deck plug-in.");
                     return;
                 }
+
+                try
+                {
+                    string installDir = GetStreamDeckInstallationDirectory();
+                    if (string.IsNullOrEmpty(installDir))
+                    {
+                        await MessageBoxHelper.ShowMessageDialog("Stream Deck doesn't appear to be installed correctly.");
+                        return;
+                    }
+
+                    string streamDeckExe = Path.Combine(installDir, "StreamDeck.exe");
+                    if (!File.Exists(streamDeckExe))
+                    {
+                        await MessageBoxHelper.ShowMessageDialog("Could not find the Stream Deck application in the installation directory.");
+                        return;
+                    }
+
+                    FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(streamDeckExe);
+                    if (versionInfo.FileMajorPart < 4)
+                    {
+                        await MessageBoxHelper.ShowMessageDialog("Stream Deck is installed, but is not v4. Please update Stream Deck to v4.");
+                        return;
+                    }
+                }
+                catch (Exception ex) { Logger.Log(ex); }
 
                 Process.Start("com.mixitup.streamdeckplugin.streamDeckPlugin");
             });
