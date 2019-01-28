e1f9b253cc0acd1d15404a8fa22c8da35ef2b4d4
Shane DeSeranno <tyren@syntheticstream.com>
2019-01-28 11:56:43 -0800

    Add IsStreamer and IsPartnered tracking to login telemetry

--- MixItUp.Base/ChannelSession.cs
+++ MixItUp.Base/ChannelSession.cs
@@ -626,7 +626,7 @@ namespace MixItUp.Base
 
                     await ChannelSession.Services.Settings.PerformBackupIfApplicable(ChannelSession.Settings);
 
-                    ChannelSession.Services.Telemetry.TrackLogin();
+                    ChannelSession.Services.Telemetry.TrackLogin(ChannelSession.IsStreamer, ChannelSession.Channel.partnered);
                     if (ChannelSession.Settings.IsStreamer)
                     {
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
