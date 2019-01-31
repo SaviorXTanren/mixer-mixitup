06d9c66bbb8a9364074cac26795a68b2c00a181e
Matthew Olivo <saviorxtanren@gmail.com>
2019-01-31 14:02:27 -0800

    - Preventing any user @mentions for Twitter action

--- MixItUp.Base/Actions/TwitterAction.cs
+++ MixItUp.Base/Actions/TwitterAction.cs
@@ -19,7 +19,7 @@ namespace MixItUp.Base.Actions
     [DataContract]
     public class TwitterAction : ActionBase
     {
-        public static bool CheckIfTweetContainsTooManyTags(string tweet) { return !string.IsNullOrEmpty(tweet) && tweet.Count(c => c == '@') > 1; }
+        public static bool CheckIfTweetContainsTooManyTags(string tweet) { return !string.IsNullOrEmpty(tweet) && tweet.Count(c => c == '@') > 0; }
 
         private static SemaphoreSlim asyncSemaphore = new SemaphoreSlim(1);
 
