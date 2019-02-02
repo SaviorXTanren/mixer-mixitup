167eb2cd37358e913a0b2602ca644447be0d9fc1
Matthew Olivo <saviorxtanren@gmail.com>
2019-02-02 13:34:12 -0800

    - Adding initial remote service

---
+++ MixItUp.Base/Model/Remote/RemoteProfileModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MixItUp.Base.Remote.Models
+{
+    [DataContract]
+    public class RemoteProfileModel
+    {
+        [DataMember]
+        public Guid ID { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        public RemoteProfileModel() { }
+
+        public RemoteProfileModel(string name)
+            : base()
+        {
+            this.ID = Guid.NewGuid();
+            this.Name = name;
+        }
+    }
+
+    [DataContract]
+    public class RemoteProfileBoardModel
+    {
+        [DataMember]
+        public RemoteProfileModel Profile { get; set; }
+
+        [DataMember]
+        public RemoteBoardModel Board { get; set; }
+    }
+}
---
+++ MixItUp.Base/Services/IRemoteService.cs
@@ -0,0 +1,57 @@
+﻿using MixItUp.Base.Remote.Models;
+using MixItUp.Base.Util;
+using MixItUp.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MixItUp.Base.Services
+{
+    public abstract class RemoteServiceBase : IRemoteService
+    {
+        public const string ReceiveProfilesMethodName = "ReceiveProfiles";
+        public const string ReceiveProfileBoardMethodName = "ReceiveProfileBoard";
+        public const string ReceiveCommandMethodName = "ReceiveCommand";
+
+        public const string SendProfilesMethodName = "SendProfiles";
+        public const string SendProfileBoardMethodName = "SendProfileBoard";
+        public const string SendCommandMethodName = "SendCommand";
+
+        private SignalRConnection connection;
+
+        public RemoteServiceBase(SignalRConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public void ListenForReceiveProfiles(Action<IEnumerable<RemoteProfileModel>> action) { this.connection.Listen(ReceiveProfilesMethodName, action); }
+
+        public void ListenForReceiveProfileBoard(Action<RemoteProfileBoardModel> action) { this.connection.Listen(ReceiveProfileBoardMethodName, action); }
+
+        public void ListenForReceiveCommand(Action<Guid> action) { this.connection.Listen(ReceiveCommandMethodName, action); }
+
+        public async Task SendProfiles(IEnumerable<RemoteProfileModel> profiles) { await this.AsyncWrapper(this.connection.Send(SendProfilesMethodName, profiles)); }
+
+        public async Task SendProfileBoard(RemoteProfileBoardModel profileBoard) { await this.AsyncWrapper(this.connection.Send(SendProfileBoardMethodName, profileBoard)); }
+
+        public async Task SendCommand(Guid commandID) { await this.AsyncWrapper(this.connection.Send(SendCommandMethodName, commandID)); }
+
+        private async Task AsyncWrapper(Task task)
+        {
+            try
+            {
+                await task;
+            }
+            catch (Exception ex) { Logger.Log(ex); }
+        }
+    }
+
+    public interface IRemoteService
+    {
+        Task SendProfiles(IEnumerable<RemoteProfileModel> profiles);
+
+        Task SendProfileBoard(RemoteProfileBoardModel profileBoard);
+
+        Task SendCommand(Guid commandID);
+    }
+}
