101340d0fb1c2a26d13613817a69317983dec914
Matthew Olivo <saviorxtanren@gmail.com>
2019-02-08 14:05:39 -0800

    - Adding initial GIF skill pop-out

---
+++ MixItUp.WPF/Controls/Chat/GifSkillPopoutControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MixItUp.WPF.Controls.Chat
+{
+    /// <summary>
+    /// Interaction logic for GifSkillPopoutControl.xaml
+    /// </summary>
+    public partial class GifSkillPopoutControl : UserControl
+    {
+        public GifSkillPopoutControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
