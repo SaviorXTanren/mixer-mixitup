ef79b68fde03c29949722341c407c7bd85539766
Shane DeSeranno <tyren@syntheticstream.com>
2019-04-12 17:55:26 -0700

    Reduce the number of Spotify API calls we make by 75%

--- MixItUp.Desktop/Services/SongRequestService.cs
+++ MixItUp.Desktop/Services/SongRequestService.cs
@@ -569,7 +569,7 @@ namespace MixItUp.Desktop.Services
                     await Task.Delay(2500, tokenSource.Token);
                 }
 
-                await Task.Delay(1000, tokenSource.Token);
+                await Task.Delay(2000, tokenSource.Token);
             });
         }
 
@@ -962,7 +962,7 @@ namespace MixItUp.Desktop.Services
                         result.State = SongRequestStateEnum.Ended;
                     }
 
-                    result.Volume = await ChannelSession.Services.Spotify.GetVolume();
+                    result.Volume = currentlyPlaying.Volume;
 
                     return result;
                 }
