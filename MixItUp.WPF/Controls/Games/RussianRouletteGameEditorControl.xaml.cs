c75c584c9b6b7699c7ae7839b1ab37297122c0b4
Matthew Olivo <saviorxtanren@gmail.com>
2019-05-24 18:13:10 -0700

    - MVVM-ing Russian Roulette game command

--- MixItUp.WPF/Controls/Games/RussianRouletteGameEditorControl.xaml.cs
+++ MixItUp.WPF/Controls/Games/RussianRouletteGameEditorControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using MixItUp.Base;
 using MixItUp.Base.Commands;
+using MixItUp.Base.ViewModel.Controls.Games;
 using MixItUp.Base.ViewModel.Requirement;
 using MixItUp.Base.ViewModel.User;
 using MixItUp.WPF.Util;
@@ -14,26 +15,22 @@ namespace MixItUp.WPF.Controls.Games
     /// </summary>
     public partial class RussianRouletteGameEditorControl : GameEditorControlBase
     {
+        private RussianRouletteGameControlViewModel viewModel;
         private RussianRouletteGameCommand existingCommand;
 
-        private CustomCommand startedCommand { get; set; }
-
-        private CustomCommand userJoinCommand { get; set; }
-
-        private CustomCommand userSuccessCommand { get; set; }
-        private CustomCommand userFailCommand { get; set; }
-
-        private CustomCommand gameCompleteCommand { get; set; }
-
-        public RussianRouletteGameEditorControl()
+        public RussianRouletteGameEditorControl(UserCurrencyViewModel currency)
         {
             InitializeComponent();
+
+            this.viewModel = new RussianRouletteGameControlViewModel(currency);
         }
 
         public RussianRouletteGameEditorControl(RussianRouletteGameCommand command)
-            : this()
         {
+            InitializeComponent();
+
             this.existingCommand = command;
+            this.viewModel = new RussianRouletteGameControlViewModel(command);
         }
 
         public override async Task<bool> Validate()
@@ -42,97 +39,28 @@ namespace MixItUp.WPF.Controls.Games
             {
                 return false;
             }
-
-            if (!int.TryParse(this.TimeLimitTextBox.Text, out int timeLimit) || timeLimit <= 0)
-            {
-                await MessageBoxHelper.ShowMessageDialog("The Time Limit is not a valid number greater than 0");
-                return false;
-            }
-
-            if (!int.TryParse(this.MinimumParticipantsTextBox.Text, out int minimumParticipants) || minimumParticipants <= 0)
-            {
-                await MessageBoxHelper.ShowMessageDialog("The Minimum Participants is not a valid number greater than 0");
-                return false;
-            }
-
-            if (!int.TryParse(this.MaxWinnersTextBox.Text, out int maxWinners) || maxWinners <= 0)
-            {
-                await MessageBoxHelper.ShowMessageDialog("The Max Winners is not a valid number greater than 0");
-                return false;
-            }
-
-            if (maxWinners >= minimumParticipants)
-            {
-                await MessageBoxHelper.ShowMessageDialog("Max Winners must be less than Minimum Participants");
-                return false;
-            }
-
-            return true;
+            return await this.viewModel.Validate();
         }
 
         public override void SaveGameCommand()
         {
-            int.TryParse(this.MinimumParticipantsTextBox.Text, out int minimumParticipants);
-            int.TryParse(this.TimeLimitTextBox.Text, out int timeLimit);
-            int.TryParse(this.MaxWinnersTextBox.Text, out int maxWinners);
-
-            Dictionary<MixerRoleEnum, int> roleProbabilities = new Dictionary<MixerRoleEnum, int>() { { MixerRoleEnum.User, 0 }, { MixerRoleEnum.Subscriber, 0 }, { MixerRoleEnum.Mod, 0 } };
-
-            GameCommandBase newCommand = new RussianRouletteGameCommand(this.CommandDetailsControl.GameName, this.CommandDetailsControl.ChatTriggers,
-                this.CommandDetailsControl.GetRequirements(), minimumParticipants, timeLimit, this.startedCommand, this.userJoinCommand,
-                new GameOutcome("Success", 0, roleProbabilities, this.userSuccessCommand), new GameOutcome("Failure", 0, roleProbabilities, this.userFailCommand), maxWinners, this.gameCompleteCommand);
-            if (this.existingCommand != null)
-            {
-                ChannelSession.Settings.GameCommands.Remove(this.existingCommand);
-                newCommand.ID = this.existingCommand.ID;
-            }
-            ChannelSession.Settings.GameCommands.Add(newCommand);
+            this.viewModel.SaveGameCommand(this.CommandDetailsControl.GameName, this.CommandDetailsControl.ChatTriggers, this.CommandDetailsControl.GetRequirements());
         }
 
-        protected override Task OnLoaded()
+        protected override async Task OnLoaded()
         {
+            this.DataContext = this.viewModel;
+            await this.viewModel.OnLoaded();
+
             if (this.existingCommand != null)
             {
                 this.CommandDetailsControl.SetDefaultValues(this.existingCommand);
-
-                this.MinimumParticipantsTextBox.Text = this.existingCommand.MinimumParticipants.ToString();
-                this.TimeLimitTextBox.Text = this.existingCommand.TimeLimit.ToString();
-                this.MaxWinnersTextBox.Text = this.existingCommand.MaxWinners.ToString();
-
-                this.startedCommand = this.existingCommand.StartedCommand;
-
-                this.userJoinCommand = this.existingCommand.UserJoinCommand;
-
-                this.userSuccessCommand = this.existingCommand.UserSuccessOutcome.Command;
-                this.userFailCommand = this.existingCommand.UserFailOutcome.Command;
-
-                this.gameCompleteCommand = this.existingCommand.GameCompleteCommand;
             }
             else
             {
                 this.CommandDetailsControl.SetDefaultValues("Russian Roulette", "rr russian", CurrencyRequirementTypeEnum.MinimumAndMaximum, 10, 1000);
-                UserCurrencyViewModel currency = ChannelSession.Settings.Currencies.Values.FirstOrDefault();
-                this.MinimumParticipantsTextBox.Text = "2";
-                this.TimeLimitTextBox.Text = "30";
-                this.MaxWinnersTextBox.Text = "1";
-
-                this.startedCommand = this.CreateBasicChatCommand("@$username has started a game of Russian Roulette with a $gamebet " + currency.Name + " entry fee! Type !rr to join in!");
-
-                this.userJoinCommand = this.CreateBasicChatCommand("You've joined in the russian roulette match! Let's see who walks away the winner...", whisper: true);
-
-                this.userSuccessCommand = this.CreateBasicChatCommand("You survived and walked away with $gamepayout " + currency.Name + "!", whisper: true);
-                this.userFailCommand = this.CreateBasicChatCommand("Looks like luck was not on your side. Better luck next time...", whisper: true);
-
-                this.gameCompleteCommand = this.CreateBasicChatCommand("The dust settles after a grueling match-up and...It's $gamewinners! Total Amount Per Winner: $gameallpayout " + currency.Name + "!");
             }
-
-            this.GameStartCommandButtonsControl.DataContext = this.startedCommand;
-            this.UserJoinedCommandButtonsControl.DataContext = this.userJoinCommand;
-            this.SuccessOutcomeCommandButtonsControl.DataContext = this.userSuccessCommand;
-            this.FailOutcomeCommandButtonsControl.DataContext = this.userFailCommand;
-            this.GameCompleteCommandButtonsControl.DataContext = this.gameCompleteCommand;
-
-            return base.OnLoaded();
+            await base.OnLoaded();
         }
     }
 }
--- MixItUp.WPF/Windows/Command/GameCommandWindow.xaml.cs
+++ MixItUp.WPF/Windows/Command/GameCommandWindow.xaml.cs
@@ -64,7 +64,7 @@ namespace MixItUp.WPF.Windows.Command
                 this.gameEditors.Add("Pickpocket", new PickpocketGameEditorControl(this.viewModel.DefaultCurrency));
                 this.gameEditors.Add("Duel", new DuelGameEditorControl(this.viewModel.DefaultCurrency));
                 this.gameEditors.Add("Heist", new HeistGameEditorControl(this.viewModel.DefaultCurrency));
-                this.gameEditors.Add("Russian Roulette", new RussianRouletteGameEditorControl());
+                this.gameEditors.Add("Russian Roulette", new RussianRouletteGameEditorControl(this.viewModel.DefaultCurrency));
                 this.gameEditors.Add("Roulette", new RouletteGameEditorControl(this.viewModel.DefaultCurrency));
                 this.gameEditors.Add("Bid", new BidGameEditorControl(this.viewModel.DefaultCurrency));
                 this.gameEditors.Add("Hitman", new HitmanGameEditorControl(this.viewModel.DefaultCurrency));
