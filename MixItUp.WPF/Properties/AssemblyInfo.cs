7b6c58aaf9440d5b99d48c57761a6f1f4ed586e4
Matthew Olivo <saviorxtanren@gmail.com>
2019-08-10 15:13:57 -0700

    - Updating to 0.5.2.7

--- MixItUp.WPF/Properties/AssemblyInfo.cs
+++ MixItUp.WPF/Properties/AssemblyInfo.cs
@@ -48,4 +48,4 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.5.2.6")]
+[assembly: AssemblyVersion("0.5.2.7")]
