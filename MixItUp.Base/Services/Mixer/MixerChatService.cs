13323e1b386c853db978c6d8e7327d828b3010cd
Matthew Olivo <saviorxtanren@gmail.com>
2019-09-21 14:49:54 -0700

    - Re-adding background chat user refresh logic

--- MixItUp.Base/Services/Mixer/MixerChatService.cs
+++ MixItUp.Base/Services/Mixer/MixerChatService.cs
@@ -505,39 +505,45 @@ namespace MixItUp.Base.Services.Mixer
 
         private async Task ChatterRefreshBackground(CancellationToken cancellationToken)
         {
-            //IEnumerable<ChatUserModel> chatUsers = await ChannelSession.MixerStreamerConnection.GetChatUsers(ChannelSession.MixerChannel, int.MaxValue);
-            //chatUsers = chatUsers.Where(u => u.userId.HasValue);
-            //HashSet<uint> chatUserIDs = new HashSet<uint>(chatUsers.Select(u => u.userId.GetValueOrDefault()));
+            List<ChatUserModel> chatUsers = new List<ChatUserModel>();
+            await ChannelSession.MixerStreamerConnection.GetChatUsers(ChannelSession.MixerChannel, (users) =>
+            {
+                chatUsers.AddRange(users);
+                return Task.FromResult(0);
+            }, uint.MaxValue);
 
-            //IEnumerable<UserViewModel> existingUsers = await ChannelSession.ActiveUsers.GetAllUsers();
-            //HashSet<uint> existingUsersIDs = new HashSet<uint>(existingUsers.Select(u => u.ID));
+            chatUsers = chatUsers.Where(u => u.userId.HasValue).ToList();
+            HashSet<uint> chatUserIDs = new HashSet<uint>(chatUsers.Select(u => u.userId.GetValueOrDefault()));
 
-            //Dictionary<uint, ChatUserModel> usersToAdd = chatUsers.ToDictionary(u => u.userId.GetValueOrDefault(), u => u);
-            //List<uint> usersToRemove = new List<uint>();
+            IEnumerable<UserViewModel> existingUsers = ChannelSession.Services.User.GetAllUsers();
+            HashSet<uint> existingUsersIDs = new HashSet<uint>(existingUsers.Select(u => u.ID));
 
-            //foreach (uint userID in existingUsersIDs)
-            //{
-            //    usersToAdd.Remove(userID);
-            //    if (!chatUserIDs.Contains(userID))
-            //    {
-            //        usersToRemove.Add(userID);
-            //    }
-            //}
+            Dictionary<uint, ChatUserModel> usersToAdd = chatUsers.ToDictionary(u => u.userId.GetValueOrDefault(), u => u);
+            List<uint> usersToRemove = new List<uint>();
 
-            //foreach (ChatUserModel user in usersToAdd.Values)
-            //{
-            //    this.ChatClient_OnUserJoinOccurred(this, new ChatUserEventModel()
-            //    {
-            //        id = user.userId.GetValueOrDefault(),
-            //        username = user.userName,
-            //        roles = user.userRoles,
-            //    });
-            //}
+            foreach (uint userID in existingUsersIDs)
+            {
+                usersToAdd.Remove(userID);
+                if (!chatUserIDs.Contains(userID))
+                {
+                    usersToRemove.Add(userID);
+                }
+            }
 
-            //foreach (uint userID in usersToRemove)
-            //{
-            //    this.ChatClient_OnUserLeaveOccurred(this, new ChatUserEventModel() { id = userID });
-            //}
+            foreach (ChatUserModel user in usersToAdd.Values)
+            {
+                this.ChatClient_OnUserJoinOccurred(this, new ChatUserEventModel()
+                {
+                    id = user.userId.GetValueOrDefault(),
+                    username = user.userName,
+                    roles = user.userRoles,
+                });
+            }
+
+            foreach (uint userID in usersToRemove)
+            {
+                this.ChatClient_OnUserLeaveOccurred(this, new ChatUserEventModel() { id = userID });
+            }
         }
 
         #endregion Refresh Methods
