9c11e0af94271922793bfec12da8650a1dbb324c
Shane DeSeranno <tyren@syntheticstream.com>
2019-11-13 19:34:39 -0800

    Remove unused typescript API prototype

--- APIs/mixitup-typescript-api/lib/models/giveusercurrency.ts
+++
@@ -1,4 +0,0 @@
-export interface IGiveUserCurrency {
-    Amount: number;
-    UsernameOrID: string;
-}
--- APIs/mixitup-typescript-api/lib/models/sendchatmessage.ts
+++
@@ -1,4 +0,0 @@
-export interface ISendChatMessage {
-    Message: string;
-    SendAsStreamer: boolean;
-}
