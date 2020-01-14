7bb3a216d6e80444e79eb3d36bf7d8d519e58773
Matthew Olivo <saviorxtanren@gmail.com>
2020-01-13 16:35:10 -0800

    - Additional Song Request dependency removal

--- MixItUp.WPF/Controls/MainControls/SongRequestControl.xaml.cs
+++ MixItUp.WPF/Controls/MainControls/SongRequestControl.xaml.cs
@@ -1,24 +1,10 @@
-﻿using MixItUp.Base;
-using MixItUp.Base.Commands;
-using MixItUp.Base.Model.SongRequests;
-using MixItUp.Base.ViewModel.Controls.MainControls;
-using MixItUp.Base.ViewModel.Window;
-using MixItUp.WPF.Controls.Command;
-using MixItUp.WPF.Windows.Command;
-using System;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-
-namespace MixItUp.WPF.Controls.MainControls
+﻿namespace MixItUp.WPF.Controls.MainControls
 {
     /// <summary>
     /// Interaction logic for SongRequestControl.xaml
     /// </summary>
     public partial class SongRequestControl : MainControlBase
     {
-        private SongRequestsMainControlViewModel viewModel;
-
         public SongRequestControl()
         {
             InitializeComponent();
