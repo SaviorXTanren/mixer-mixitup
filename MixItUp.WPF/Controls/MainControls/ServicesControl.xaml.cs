356b6ff837c062cc243abb4eaa8607f2be196f50
Matthew Olivo <saviorxtanren@gmail.com>
2020-01-16 02:15:06 -0800

    - Migrating Stream Deck service to new format

--- MixItUp.WPF/Controls/MainControls/ServicesControl.xaml.cs
+++ MixItUp.WPF/Controls/MainControls/ServicesControl.xaml.cs
@@ -40,7 +40,7 @@ namespace MixItUp.WPF.Controls.MainControls
             this.services.Add(new ServiceContainerControl(this.Window, new JustGivingServiceControl()));
             this.services.Add(new ServicesGroupBoxControl(this.Window, new TwitterServiceControl()));
             this.services.Add(new ServicesGroupBoxControl(this.Window, new DiscordServiceControl()));
-            this.services.Add(new ServicesGroupBoxControl(this.Window, new StreamDeckServiceControl()));
+            this.services.Add(new ServiceContainerControl(this.Window, new StreamDeckServiceControl()));
             this.services.Add(new ServiceContainerControl(this.Window, new IFTTTServiceControl()));
             this.services.Add(new ServiceContainerControl(this.Window, new StreamAvatarsServiceControl()));
             this.services.Add(new ServicesGroupBoxControl(this.Window, new DeveloperAPIServiceControl()));
