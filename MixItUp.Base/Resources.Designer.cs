5473fad477b02c2f0a7305b9a2e2b16ba862b205
Matthew Olivo <saviorxtanren@gmail.com>
2020-02-12 19:29:14 -0800

    - Adding resources

--- MixItUp.Base/Resources.Designer.cs
+++ MixItUp.Base/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace MixItUp.Base {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -57,6 +57,15 @@ namespace MixItUp.Base {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 1 Per Bit.
+        /// </summary>
+        public static string _1PerBit {
+            get {
+                return ResourceManager.GetString("1PerBit", resourceCulture);
             }
         }
         
