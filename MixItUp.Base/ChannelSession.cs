028b282f9e08f77c4b5ed1bffdbf191b04f71c27
Matthew Olivo <saviorxtanren@gmail.com>
2020-02-29 14:11:48 -0800

    - Adding additional diagnostic logging around settings & application closing

--- MixItUp.Base/ChannelSession.cs
+++ MixItUp.Base/ChannelSession.cs
@@ -507,7 +507,7 @@ namespace MixItUp.Base
                 ChannelSession.Services.InputService.HotKeyPressed += InputService_HotKeyPressed;
 
                 await ChannelSession.SaveSettings();
-                await ChannelSession.Services.Settings.PerformBackupIfApplicable(ChannelSession.Settings);
+                await ChannelSession.Services.Settings.PerformAutomaticBackupIfApplicable(ChannelSession.Settings);
 
                 ChannelSession.Services.Telemetry.TrackLogin(ChannelSession.MixerUser.id.ToString(), ChannelSession.IsStreamer, ChannelSession.MixerChannel.partnered);
                 if (ChannelSession.Settings.IsStreamer)
