ff320242bf88379fa29f8d72d7838867e6a6cee5
Matthew Olivo <saviorxtanren@gmail.com>
2020-06-08 17:46:35 -0700

    - Adding "Not Replaced" operator to Conditional Action

--- MixItUp.Base/Resources.Designer.cs
+++ MixItUp.Base/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace MixItUp.Base {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -4395,6 +4395,15 @@ namespace MixItUp.Base {
         public static string Notifications {
             get {
                 return ResourceManager.GetString("Notifications", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Not Replaced.
+        /// </summary>
+        public static string NotReplaced {
+            get {
+                return ResourceManager.GetString("NotReplaced", resourceCulture);
             }
         }
         
