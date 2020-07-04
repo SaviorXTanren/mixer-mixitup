81966d0d9249994d9f9bb4949a234d6a5ad5c4b5
Matthew Olivo <saviorxtanren@gmail.com>
2020-07-04 00:01:45 -0700

    - Adding Changelog page

---
+++ MixItUp.WPF/Controls/MainControls/ChangelogControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace MixItUp.WPF.Controls.MainControls
+{
+    /// <summary>
+    /// Interaction logic for ChangelogControl.xaml
+    /// </summary>
+    public partial class ChangelogControl : MainControlBase
+    {
+        public ChangelogControl()
+        {
+            InitializeComponent();
+        }
+
+        protected override Task InitializeInternal()
+        {
+            this.ChangelogWebBrowser.Navigate("file:///" + Path.GetFullPath("Changelog.html"));
+            return base.InitializeInternal();
+        }
+    }
+}
