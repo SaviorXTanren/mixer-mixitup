e882d18aa4d14c96fc21108818c71dca1d09ec31
Matthew Olivo <saviorxtanren@gmail.com>
2020-08-03 14:54:14 -0700

    - Adding invariant culture parsing for Twitch datetimes

--- MixItUp.Base/Services/Twitch/TwitchPlatformService.cs
+++ MixItUp.Base/Services/Twitch/TwitchPlatformService.cs
@@ -137,12 +137,12 @@ namespace MixItUp.Base.Services.Twitch
             {
                 if (dateTime.Contains("Z", StringComparison.InvariantCultureIgnoreCase))
                 {
-                    if (DateTimeOffset.TryParse(dateTime, out DateTimeOffset startUTC))
+                    if (DateTimeOffset.TryParse(dateTime, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal, out DateTimeOffset startUTC))
                     {
                         return startUTC.ToCorrectLocalTime();
                     }
                 }
-                else if (DateTime.TryParse(dateTime, out DateTime start))
+                else if (DateTime.TryParse(dateTime, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal, out DateTime start))
                 {
                     return new DateTimeOffset(start, TimeSpan.Zero).ToCorrectLocalTime();
                 }
