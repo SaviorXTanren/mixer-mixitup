c65d7ae918b221333c1785051355011396e32058
Matthew Olivo <saviorxtanren@gmail.com>
2020-09-10 23:52:53 -0700

    - Fixing bug with OvrStream action not being assigned the right control

--- MixItUp.WPF/Controls/Actions/ActionEditorContainerControl.xaml.cs
+++ MixItUp.WPF/Controls/Actions/ActionEditorContainerControl.xaml.cs
@@ -46,8 +46,8 @@ namespace MixItUp.WPF.Controls.Actions
                     case ActionTypeEnum.IFTTT: this.Content = new IFTTTActionEditorControl(); break;
                     case ActionTypeEnum.Input: this.Content = new InputActionEditorControl(); break;
                     case ActionTypeEnum.Moderation: this.Content = new ModerationActionEditorControl(); break;
-                    case ActionTypeEnum.Overlay: this.Content = new OvrStreamActionEditorControl(); break;
-                    case ActionTypeEnum.OvrStream: break;
+                    case ActionTypeEnum.Overlay: 
+                    case ActionTypeEnum.OvrStream: this.Content = new OvrStreamActionEditorControl(); break;
                     case ActionTypeEnum.Serial: break;
                     case ActionTypeEnum.Sound: break;
                     case ActionTypeEnum.SpecialIdentifier: break;
