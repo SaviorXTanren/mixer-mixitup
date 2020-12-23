59da578d2e11c962af1ed94e9b720f7cc545838a
Matthew Olivo <saviorxtanren@gmail.com>
2020-12-23 00:41:23 -0800

    - Adding logic to hard set currency required amount once game has started for Hitman, Russian Roulette, Treasure Defense, & Word Scramble

--- MixItUp.Base/Model/Commands/Games/TreasureDefenseGameCommandModel.cs
+++ MixItUp.Base/Model/Commands/Games/TreasureDefenseGameCommandModel.cs
@@ -120,6 +120,7 @@ namespace MixItUp.Base.Model.Commands.Games
                 this.runBetAmount = this.GetPrimaryBetAmount(parameters);
                 this.runParameters = parameters;
                 this.runUsers[parameters.User] = parameters;
+                this.GetPrimaryCurrencyRequirement().SetTemporaryAmount(this.runBetAmount);
 
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                 AsyncRunner.RunAsyncBackground(async (cancellationToken) =>
@@ -242,6 +243,7 @@ namespace MixItUp.Base.Model.Commands.Games
             this.runBetAmount = 0;
             this.runUsers.Clear();
             this.runUserTypes.Clear();
+            this.GetPrimaryCurrencyRequirement().ResetTemporaryAmount();
         }
     }
 }
--- MixItUp.Base/Model/Commands/Games/WordScrambleGameCommandModel.cs
+++ MixItUp.Base/Model/Commands/Games/WordScrambleGameCommandModel.cs
@@ -110,6 +110,7 @@ namespace MixItUp.Base.Model.Commands.Games
                 this.runBetAmount = this.GetPrimaryBetAmount(parameters);
                 this.runParameters = parameters;
                 this.runUsers[parameters.User] = parameters;
+                this.GetPrimaryCurrencyRequirement().SetTemporaryAmount(this.runBetAmount);
 
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                 AsyncRunner.RunAsyncBackground(async (cancellationToken) =>
@@ -194,6 +195,7 @@ namespace MixItUp.Base.Model.Commands.Games
             this.runWord = null;
             this.runWordScrambled = null;
             this.runUsers.Clear();
+            this.GetPrimaryCurrencyRequirement().ResetTemporaryAmount();
         }
     }
 }
