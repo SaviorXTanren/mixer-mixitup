40911767699b3364c567832a53271196890d3b04
Matthew Olivo <saviorxtanren@gmail.com>
2021-01-06 04:36:39 -0800

    - Adding edit command button to Command Action editor

--- MixItUp.WPF/Controls/Actions/CommandActionEditorControl.xaml.cs
+++ MixItUp.WPF/Controls/Actions/CommandActionEditorControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace MixItUp.WPF.Controls.Actions
+﻿using MixItUp.Base.ViewModel.Actions;
+using MixItUp.WPF.Windows.Commands;
+
+namespace MixItUp.WPF.Controls.Actions
 {
     /// <summary>
     /// Interaction logic for CommandActionEditorControl.xaml
@@ -8,6 +11,16 @@
         public CommandActionEditorControl()
         {
             InitializeComponent();
+        }
+
+        private void EditCommandButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            CommandActionEditorControlViewModel viewModel = (CommandActionEditorControlViewModel)this.DataContext;
+            if (viewModel != null && viewModel.SelectedCommand != null)
+            {
+                CommandEditorWindow window = new CommandEditorWindow(viewModel.SelectedCommand);
+                window.Show();
+            }
         }
     }
 }
