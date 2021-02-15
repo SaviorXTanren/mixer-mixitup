ff933f14cfa7cd0b91ee65cb115beab94040fba6
Matthew Olivo <saviorxtanren@gmail.com>
2021-02-14 17:16:40 -0800

    - Adding clickable link to Counters folder in Counter Action

--- MixItUp.WPF/Controls/Actions/CounterActionEditorControl.xaml.cs
+++ MixItUp.WPF/Controls/Actions/CounterActionEditorControl.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace MixItUp.WPF.Controls.Actions
+﻿using MixItUp.Base;
+using MixItUp.Base.Model.Settings;
+using MixItUp.Base.Util;
+using System.IO;
+
+namespace MixItUp.WPF.Controls.Actions
 {
     /// <summary>
     /// Interaction logic for CounterActionEditorControl.xaml
@@ -8,6 +13,16 @@
         public CounterActionEditorControl()
         {
             InitializeComponent();
+        }
+
+        private async void CounterFolderHyperlink_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            string counterFolderPath = Path.Combine(ChannelSession.Services.FileService.GetApplicationDirectory(), CounterModel.CounterFolderName);
+            if (!Directory.Exists(counterFolderPath))
+            {
+                await ChannelSession.Services.FileService.CreateDirectory(counterFolderPath);
+            }
+            ProcessHelper.LaunchFolder(counterFolderPath);
         }
     }
 }
