3d6ffcabf0d7533b43d7ddb4caac5dfd01de71d7
Matthew Olivo <saviorxtanren@gmail.com>
2021-02-25 02:39:29 -0800

    - Adding Glimesh chat message view models

--- MixItUp.Base/ViewModel/Chat/Glimesh/GlimeshChatMessageViewModel.cs
+++ MixItUp.Base/ViewModel/Chat/Glimesh/GlimeshChatMessageViewModel.cs
@@ -1,48 +1,32 @@
 ﻿using Glimesh.Base.Models.Clients.Chat;
 using MixItUp.Base.Model;
 using MixItUp.Base.ViewModel.User;
+using System;
 
 namespace MixItUp.Base.ViewModel.Chat.Glimesh
 {
     public class GlimeshChatMessageViewModel : UserChatMessageViewModel
     {
         public GlimeshChatMessageViewModel(ChatMessagePacketModel message, UserViewModel user = null)
-            : base(null, StreamingPlatformTypeEnum.Glimesh, (user != null) ? user : new UserViewModel(message))
+            : base(message.ID, StreamingPlatformTypeEnum.Glimesh, (user != null) ? user : new UserViewModel(message))
         {
-            //this.User.SetTwitchChatDetails(message);
-
-            //foreach (var kvp in message.EmotesDictionary)
-            //{
-            //    if (!messageEmotesHashSet.Contains(kvp.Key) && kvp.Value.Count > 0)
-            //    {
-            //        long emoteID = kvp.Key;
-            //        Tuple<int, int> instance = kvp.Value.FirstOrDefault();
-            //        if (0 <= instance.Item1 && instance.Item1 < message.Message.Length && 0 <= instance.Item2 && instance.Item2 < message.Message.Length)
-            //        {
-            //            string emoteCode = message.Message.Substring(instance.Item1, instance.Item2 - instance.Item1 + 1);
-            //            messageEmotesCache[emoteCode] = new EmoteModel()
-            //            {
-            //                id = emoteID,
-            //                code = emoteCode
-            //            };
-            //            messageEmotesHashSet.Add(kvp.Key);
-            //        }
-            //    }
-            //}
-
-            //this.HasBits = (int.TryParse(message.Bits, out int bits) && bits > 0);
-            //this.IsHighlightedMessage = message.RawPacket.Tags.ContainsKey(TagMessageID) && message.RawPacket.Tags[TagMessageID].Equals(MessageIDHighlightedMessage);
-
-            //if (message.Message.StartsWith(SlashMeAction) && message.Message.Last() == SOHCharacter)
-            //{
-            //    this.IsSlashMe = true;
-            //    string text = message.Message.Replace(SlashMeAction, string.Empty);
-            //    this.ProcessMessageContents(text.Substring(0, text.Length - 1));
-            //}
-            //else
-            //{
-            //    this.ProcessMessageContents(message.Message);
-            //}
+            foreach (ChatMessageTokenModel token in message.MessageTokens)
+            {
+                if (string.Equals(token.type, "text", StringComparison.OrdinalIgnoreCase))
+                {
+                    this.AddStringMessagePart(token.text);
+                }
+                else if (string.Equals(token.type, "emote", StringComparison.OrdinalIgnoreCase))
+                {
+                    this.AddStringMessagePart(token.text);
+                    this.MessageParts[this.MessageParts.Count - 1] = new GlimeshChatEmoteViewModel(token);
+                }
+                else if (string.Equals(token.type, "url", StringComparison.OrdinalIgnoreCase))
+                {
+                    this.ContainsLink = true;
+                    this.AddStringMessagePart(token.text);
+                }
+            }
         }
     }
 }
