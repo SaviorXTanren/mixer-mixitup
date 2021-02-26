dd3aaf2ef51a3c028bf2b2a3a307abf45a51103e
Matthew Olivo <saviorxtanren@gmail.com>
2021-02-26 02:19:50 -0800

    - Adding ServiceContainer

---
+++ MixItUp.Base/Services/ServiceContainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MixItUp.Base.Services
+{
+    public static class ServiceContainer
+    {
+        private static Dictionary<Type, object> serviceContainer = new Dictionary<Type, object>();
+
+        public static void Add<T>(T service) { ServiceContainer.serviceContainer[service.GetType()] = service; }
+
+        public static T Get<T>()
+        {
+            ServiceContainer.serviceContainer.TryGetValue(typeof(T), out object service);
+            return (T)service;
+        }
+
+        public static void Remove<T>(T service) { ServiceContainer.serviceContainer.Remove(service.GetType()); }
+    }
+}
