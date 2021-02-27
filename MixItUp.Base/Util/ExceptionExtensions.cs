e4e77220dee691c2971e9de12e981fa5506d3357
Matthew Olivo <saviorxtanren@gmail.com>
2021-02-27 00:52:19 -0800

    - Adding exception unwrapping when logging crashing exception

---
+++ MixItUp.Base/Util/ExceptionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MixItUp.Base.Util
+{
+    public static class ExceptionExtensions
+    {
+        public static IEnumerable<Exception> UnwrapException(this Exception exception)
+        {
+            List<Exception> results = new List<Exception>();
+            while (exception != null)
+            {
+                results.Add(exception);
+                exception = exception.InnerException;
+            }
+            return results;
+        }
+    }
+}
