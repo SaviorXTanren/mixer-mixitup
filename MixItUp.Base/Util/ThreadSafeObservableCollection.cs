92474fe1c8de892e5aa67f51b777b45104e220ef
Matthew Olivo <saviorxtanren@gmail.com>
2021-03-05 21:06:54 -0800

    - Adding additional method overrides to ThreadSafeObservableCollection

--- MixItUp.Base/Util/ThreadSafeObservableCollection.cs
+++ MixItUp.Base/Util/ThreadSafeObservableCollection.cs
@@ -13,6 +13,8 @@ namespace MixItUp.Base.Util
 
         public ThreadSafeObservableCollection(List<T> list) : base(list) { }
 
+        public new void Add(T item) { base.Add(item); }
+
         public async Task AddAsync(T item)
         {
             await DispatcherHelper.InvokeDispatcher(() =>
@@ -21,6 +23,8 @@ namespace MixItUp.Base.Util
                 return Task.FromResult(0);
             });
         }
+
+        public new void Clear() { base.Clear(); }
 
         public async Task ClearAsync()
         {
@@ -31,6 +35,34 @@ namespace MixItUp.Base.Util
             });
         }
 
+        public new IEnumerator<T> GetEnumerator() { return base.GetEnumerator(); }
+
+        public async Task<IEnumerator<T>> GetEnumeratorAsync()
+        {
+            IEnumerator<T> result = null;
+            await DispatcherHelper.InvokeDispatcher(() =>
+            {
+                result = base.GetEnumerator();
+                return Task.FromResult(0);
+            });
+            return result;
+        }
+
+        public new int IndexOf(T item) { return base.IndexOf(item); }
+
+        public async Task<int> IndexOfAsync(T item)
+        {
+            int index = -1;
+            await DispatcherHelper.InvokeDispatcher(() =>
+            {
+                index = base.IndexOf(item);
+                return Task.FromResult(0);
+            });
+            return index;
+        }
+
+        public new void Insert(int index, T item) { base.Insert(index, item); }
+
         public async Task InsertAsync(int index, T item)
         {
             await DispatcherHelper.InvokeDispatcher(() =>
@@ -39,6 +71,8 @@ namespace MixItUp.Base.Util
                 return Task.FromResult(0);
             });
         }
+
+        public new bool Remove(T item) { return base.Remove(item); }
 
         public async Task<bool> RemoveAsync(T item)
         {
