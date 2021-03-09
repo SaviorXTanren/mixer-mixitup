740e999e3d5f5a2b52fb7e5a63d04f6fea02cc81
Matthew Olivo <saviorxtanren@gmail.com>
2021-03-09 03:12:41 -0800

    - Adding additional services

---
+++ MixItUp.Base/Services/Trovo/TrovoPlatformService.cs
@@ -0,0 +1,13 @@
+﻿namespace MixItUp.Base.Services.Trovo
+{
+    public interface ITrovoPlatformService
+    { 
+    }
+
+    public class TrovoPlatformService : StreamingPlatformServiceBase, ITrovoPlatformService
+    {
+        public const string clientID = "8FMjuk785AX4FMyrwPTU3B8vYvgHWN33";
+
+        public override string Name { get { return "Trovo Connection"; } }
+    }
+}
---
+++ MixItUp.Base/Services/YouTube/YouTubePlatformService.cs
@@ -0,0 +1,14 @@
+﻿namespace MixItUp.Base.Services.YouTube
+{
+    public interface IYouTubePlatformService
+    {
+
+    }
+
+    public class YouTubePlatformService : StreamingPlatformServiceBase, IYouTubePlatformService
+    {
+        public const string ClientID = "284178717531-kago2rk85ip02qb0vmlo8898m17s6oo8.apps.googleusercontent.com";
+
+        public override string Name { get { return "YouTube Connection"; } }
+    }
+}
