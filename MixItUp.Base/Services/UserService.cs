6b4fc1f8fd5bd76b89520d42fe9b4a6de42ef74b
Matthew Olivo <saviorxtanren@gmail.com>
2021-06-26 14:29:58 -0700

    - Fixing bug with username lookup in User service

--- MixItUp.Base/Services/UserService.cs
+++ MixItUp.Base/Services/UserService.cs
@@ -42,7 +42,7 @@ namespace MixItUp.Base.Services
             if (!string.IsNullOrEmpty(username))
             {
                 username = this.SanitizeUsername(username);
-                if (platform == StreamingPlatformTypeEnum.None)
+                if (platform == StreamingPlatformTypeEnum.None || platform == StreamingPlatformTypeEnum.All)
                 {
                     foreach (StreamingPlatformTypeEnum p in StreamingPlatforms.SupportedPlatforms)
                     {
