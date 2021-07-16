ce0cf6499f70cfa1bca7c03d3f8dc22473762a2a
Matthew Olivo <saviorxtanren@gmail.com>
2021-07-16 04:03:44 -0700

    - Reducing SignalR initial connection attempts to 3

--- MixItUp.SignalR.Client/SignalRConnection.cs
+++ MixItUp.SignalR.Client/SignalRConnection.cs
@@ -32,7 +32,7 @@ namespace MixItUp.SignalR.Client
 
         public async Task<bool> Connect()
         {
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 3; i++)
             {
                 try
                 {
