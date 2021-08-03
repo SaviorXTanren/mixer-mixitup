8b21250501460857a701246af533d1cabd5af23b
Matthew Olivo <saviorxtanren@gmail.com>
2021-08-02 17:31:48 -0700

    - Fixing naming resource issue

--- MixItUp.Base/Resources.Designer.cs
+++ MixItUp.Base/Resources.Designer.cs
@@ -11236,7 +11236,7 @@ namespace MixItUp.Base {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to StreamJar Donation.
+        ///   Looks up a localized string similar to Rainmaker Donation.
         /// </summary>
         public static string RainmakerDonation {
             get {
