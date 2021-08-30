b284b2c6cdc5088f85edb82758ade0d71dea98f0
Matthew Olivo <saviorxtanren@gmail.com>
2021-08-29 19:12:02 -0700

    - Removing active user check for vip and un-vip

--- MixItUp.Base/Model/Actions/TwitchActionModel.cs
+++ MixItUp.Base/Model/Actions/TwitchActionModel.cs
@@ -254,26 +254,25 @@ namespace MixItUp.Base.Model.Actions
             }
             else if (this.ActionType == TwitchActionType.VIPUser || this.ActionType == TwitchActionType.UnVIPUser)
             {
-                UserViewModel targetUser = null;
+                string targetUsername = null;
                 if (!string.IsNullOrEmpty(this.Username))
                 {
-                    string username = await this.ReplaceStringWithSpecialModifiers(this.Username, parameters);
-                    targetUser = ChannelSession.Services.User.GetActiveUserByUsername(username, parameters.Platform);
+                    targetUsername = await this.ReplaceStringWithSpecialModifiers(this.Username, parameters);
                 }
                 else
                 {
-                    targetUser = parameters.User;
+                    targetUsername = parameters.User.TwitchUsername;
                 }
 
-                if (targetUser != null)
+                if (targetUsername != null)
                 {
                     if (this.ActionType == TwitchActionType.VIPUser)
                     {
-                        await ChannelSession.Services.Chat.SendMessage("/vip @" + targetUser.TwitchUsername, sendAsStreamer: true, platform: StreamingPlatformTypeEnum.Twitch);
+                        await ChannelSession.Services.Chat.SendMessage("/vip @" + targetUsername, sendAsStreamer: true, platform: StreamingPlatformTypeEnum.Twitch);
                     }
                     else if (this.ActionType == TwitchActionType.UnVIPUser)
                     {
-                        await ChannelSession.Services.Chat.SendMessage("/unvip @" + targetUser.TwitchUsername, sendAsStreamer: true, platform: StreamingPlatformTypeEnum.Twitch);
+                        await ChannelSession.Services.Chat.SendMessage("/unvip @" + targetUsername, sendAsStreamer: true, platform: StreamingPlatformTypeEnum.Twitch);
                     }
                 }
             }
