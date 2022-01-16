e1d4a98e3c0c65ed0b98598113230ed26b21dd25
Matthew Olivo <saviorxtanren@gmail.com>
2022-01-16 01:36:28 -0800

    - Adding filler classes for V2 API

---
+++ MixItUp.WPF/Services/DeveloperAPI/V2/ChatController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Http;
+
+namespace MixItUp.WPF.Services.DeveloperAPI.V2
+{
+    [RoutePrefix("api/v2/commands")]
+    public class ChatController : ApiController
+    {
+    }
+}
---
+++ MixItUp.WPF/Services/DeveloperAPI/V2/UsersController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Http;
+
+namespace MixItUp.WPF.Services.DeveloperAPI.V2
+{
+    [RoutePrefix("api/v2/users")]
+    public class UsersController : ApiController
+    {
+    }
+}
