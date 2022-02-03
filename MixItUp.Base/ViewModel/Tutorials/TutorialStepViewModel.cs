949e32c6935412eee2cec55d26565a9ea67e8e7a
Matthew Olivo <saviorxtanren@gmail.com>
2022-02-02 19:00:40 -0800

    - Adding initial tutorial classes

---
+++ MixItUp.Base/ViewModel/Tutorials/TutorialStepViewModel.cs
@@ -0,0 +1,45 @@
+﻿using MixItUp.Base.ViewModels;
+using System;
+
+namespace MixItUp.Base.ViewModel.Tutorials
+{
+    public class TutorialStepViewModel : UIViewModelBase
+    {
+        public string ID { get; set; }
+
+        public string Header { get; set; }
+        public string Content { get; set; }
+
+        public Action OnEntering { get; set; }
+        public Action OnEnter { get; set; }
+        public Action OnExit { get; set; }
+
+        public Action DoStep { get; set; }
+        public Func<bool> CanDoStep { get; set; } = () => { return true; };
+
+        public TutorialStepViewModel(string id, string header, string content)
+        {
+            this.ID = id;
+            this.Header = header;
+            this.Content = content;
+        }
+
+        public TutorialStepViewModel(string id, string header, string content, Action doStep)
+            : this(id, header, content)
+        {
+            this.DoStep = doStep;
+        }
+
+        public TutorialStepViewModel(string id, string header, string content, Action doStep, Func<bool> canDoStep)
+            : this(id, header, content, doStep)
+        {
+            this.CanDoStep = canDoStep;
+        }
+
+        public bool HasDo { get { return this.DoStep != null; } }
+
+        public bool HasOnEntering { get { return this.OnEntering != null; } }
+        public bool HasOnEnter { get { return this.OnEnter != null; } }
+        public bool HasOnExist { get { return this.OnExit != null; } }
+    }
+}
---
+++ MixItUp.Base/ViewModel/Tutorials/TutorialViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MixItUp.Base.ViewModel.Tutorials
+{
+    public class TutorialViewModel
+    {
+        public string Name { get; set; }
+
+        public List<TutorialStepViewModel> Steps { get; set; } = new List<TutorialStepViewModel>();
+
+        public TutorialViewModel(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
