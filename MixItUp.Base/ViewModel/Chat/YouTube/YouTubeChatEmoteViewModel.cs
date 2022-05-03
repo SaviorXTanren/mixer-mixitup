180250182d82c36cf1bf26007911b57e44acedf9
Matthew Olivo <saviorxtanren@gmail.com>
2022-05-02 22:37:11 -0700

    - Updating YT emote processing logic

--- MixItUp.Base/ViewModel/Chat/YouTube/YouTubeChatEmoteViewModel.cs
+++ MixItUp.Base/ViewModel/Chat/YouTube/YouTubeChatEmoteViewModel.cs
@@ -3,9 +3,11 @@ using System.Linq;
 
 namespace MixItUp.Base.ViewModel.Chat.YouTube
 {
-    public class YouTubeChatEmoteViewModel
+    public class YouTubeChatEmoteViewModel : IChatEmoteViewModel
     {
         public YouTubeChatEmoteModel Emote { get; set; }
+
+        public string ID { get { return this.Emote.emojiId; } }
 
         public string Name { get { return this.Emote.shortcuts?.FirstOrDefault(); } }
 
