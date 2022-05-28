9a637987119aadbbc7431ee5b97ea948ad87a07f
Matthew Olivo <saviorxtanren@gmail.com>
2022-05-27 22:57:31 -0700

    - Basics for Overlay item

--- MixItUp.Base/Model/Overlay/OverlayItemV3Model.cs
+++ MixItUp.Base/Model/Overlay/OverlayItemV3Model.cs
@@ -1,9 +1,23 @@
-﻿using System;
+﻿using MixItUp.Base.Model.Commands;
+using MixItUp.Base.Util;
+using System;
+using System.Threading.Tasks;
 
 namespace MixItUp.Base.Model.Overlay
 {
     public class OverlayItemV3Model
     {
-        public Guid ID { get; set; }
+        public string HTML { get; set; }
+        public string CSS { get; set; }
+        public string Javascript { get; set; }
+
+        public async Task<OverlayItemV3Model> GetProcessedItem(CommandParametersModel parameters)
+        {
+            OverlayItemV3Model result = new OverlayItemV3Model();
+            result.HTML = await SpecialIdentifierStringBuilder.ProcessSpecialIdentifiers(this.HTML, parameters);
+            result.CSS = await SpecialIdentifierStringBuilder.ProcessSpecialIdentifiers(this.CSS, parameters);
+            result.Javascript = await SpecialIdentifierStringBuilder.ProcessSpecialIdentifiers(this.Javascript, parameters);
+            return result;
+        }
     }
 }
