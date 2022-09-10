4ac541b10cb310fa1732775c38ad438e9b2ef15b
Matthew Olivo <saviorxtanren@gmail.com>
2022-09-10 02:26:14 -0700

    - Progress check-in of Twitch chat / command switch over

--- MixItUp.Base/Model/Actions/TwitchActionModel.cs
+++ MixItUp.Base/Model/Actions/TwitchActionModel.cs
@@ -11,11 +11,13 @@ using System.Threading;
 using System.Threading.Tasks;
 using Twitch.Base.Models.NewAPI.Ads;
 using Twitch.Base.Models.NewAPI.ChannelPoints;
+using Twitch.Base.Models.NewAPI.Chat;
 using Twitch.Base.Models.NewAPI.Clips;
 using Twitch.Base.Models.NewAPI.Polls;
 using Twitch.Base.Models.NewAPI.Predictions;
 using Twitch.Base.Models.NewAPI.Streams;
 using Twitch.Base.Models.NewAPI.Tags;
+using Twitch.Base.Models.NewAPI.Users;
 
 namespace MixItUp.Base.Model.Actions
 {
@@ -279,7 +281,11 @@ namespace MixItUp.Base.Model.Actions
                 if (this.ActionType == TwitchActionType.Raid)
                 {
                     string channelName = await ReplaceStringWithSpecialModifiers(this.Username, parameters);
-                    await ServiceManager.Get<ChatService>().SendMessage("/raid @" + channelName, sendAsStreamer: true, platform: StreamingPlatformTypeEnum.Twitch);
+                    UserModel targetChannel = await ServiceManager.Get<TwitchSessionService>().UserConnection.GetNewAPIUserByLogin(channelName);
+                    if (targetChannel != null)
+                    {
+                        await ServiceManager.Get<TwitchSessionService>().UserConnection.RaidChannel(ServiceManager.Get<TwitchSessionService>().User, targetChannel);
+                    }
                 }
                 else if (this.ActionType == TwitchActionType.RunAd)
                 {
@@ -307,13 +313,17 @@ namespace MixItUp.Base.Model.Actions
 
                     if (!string.IsNullOrEmpty(targetUsername))
                     {
-                        if (this.ActionType == TwitchActionType.VIPUser)
+                        UserModel targetUser = await ServiceManager.Get<TwitchSessionService>().UserConnection.GetNewAPIUserByLogin(targetUsername);
+                        if (targetUser != null)
                         {
-                            await ServiceManager.Get<ChatService>().SendMessage("/vip @" + targetUsername, sendAsStreamer: true, platform: StreamingPlatformTypeEnum.Twitch);
-                        }
-                        else if (this.ActionType == TwitchActionType.UnVIPUser)
-                        {
-                            await ServiceManager.Get<ChatService>().SendMessage("/unvip @" + targetUsername, sendAsStreamer: true, platform: StreamingPlatformTypeEnum.Twitch);
+                            if (this.ActionType == TwitchActionType.VIPUser)
+                            {
+                                await ServiceManager.Get<TwitchSessionService>().UserConnection.VIPUser(ServiceManager.Get<TwitchSessionService>().User, targetUser);
+                            }
+                            else if (this.ActionType == TwitchActionType.UnVIPUser)
+                            {
+                                await ServiceManager.Get<TwitchSessionService>().UserConnection.UnVIPUser(ServiceManager.Get<TwitchSessionService>().User, targetUser);
+                            }
                         }
                     }
                 }
@@ -580,43 +590,57 @@ namespace MixItUp.Base.Model.Actions
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                     }
                 }
-                else if (this.ActionType == TwitchActionType.EnableSlowChat || this.ActionType == TwitchActionType.EnableFollowersOnly)
+                else if (this.ActionType == TwitchActionType.EnableSlowChat || this.ActionType == TwitchActionType.DisableSlowChat ||
+                    this.ActionType == TwitchActionType.EnableEmoteOnly || this.ActionType == TwitchActionType.DisableEmoteOnly ||
+                    this.ActionType == TwitchActionType.EnableFollowersOnly || this.ActionType == TwitchActionType.DisableFollowersOnly ||
+                    this.ActionType == TwitchActionType.EnableSubscribersChat || this.ActionType == TwitchActionType.DisableSubscriberChat)
                 {
-                    if (int.TryParse(await ReplaceStringWithSpecialModifiers(this.TimeLength, parameters), out int timeLength) && timeLength > 0)
+                    ChatSettingsModel settings = await ServiceManager.Get<TwitchSessionService>().UserConnection.GetChatSettings(ServiceManager.Get<TwitchSessionService>().User);
+                    if (settings != null)
                     {
-                        if (this.ActionType == TwitchActionType.EnableSlowChat)
+                        if (this.ActionType == TwitchActionType.EnableSlowChat || this.ActionType == TwitchActionType.EnableFollowersOnly)
                         {
-                            await ServiceManager.Get<ChatService>().SendMessage("/slow " + timeLength, sendAsStreamer: true, platform: StreamingPlatformTypeEnum.Twitch);
+                            if (int.TryParse(await ReplaceStringWithSpecialModifiers(this.TimeLength, parameters), out int timeLength) && timeLength > 0)
+                            {
+                                if (this.ActionType == TwitchActionType.EnableSlowChat)
+                                {
+                                    settings.slow_mode = true;
+                                    settings.slow_mode_wait_time = timeLength;
+                                }
+                                else if (this.ActionType == TwitchActionType.EnableFollowersOnly)
+                                {
+                                    settings.follower_mode = true;
+                                    settings.follower_mode_duration = timeLength;
+                                }
+                            }
                         }
-                        else if (this.ActionType == TwitchActionType.EnableFollowersOnly)
+                        else if (this.ActionType == TwitchActionType.DisableSlowChat)
                         {
-                            await ServiceManager.Get<ChatService>().SendMessage("/followers " + timeLength, sendAsStreamer: true, platform: StreamingPlatformTypeEnum.Twitch);
+                            settings.slow_mode = false;
                         }
+                        else if (this.ActionType == TwitchActionType.DisableFollowersOnly)
+                        {
+                            settings.follower_mode = false;
+                        }
+                        else if (this.ActionType == TwitchActionType.EnableEmoteOnly)
+                        {
+                            settings.emote_mode = true;
+                        }
+                        else if (this.ActionType == TwitchActionType.DisableEmoteOnly)
+                        {
+                            settings.emote_mode = false;
+                        }
+                        else if (this.ActionType == TwitchActionType.EnableSubscribersChat)
+                        {
+                            settings.subscriber_mode = true;
+                        }
+                        else if (this.ActionType == TwitchActionType.DisableSubscriberChat)
+                        {
+                            settings.subscriber_mode = false;
+                        }
+
+                        await ServiceManager.Get<TwitchSessionService>().UserConnection.UpdateChatSettings(ServiceManager.Get<TwitchSessionService>().User, settings);
                     }
-                }
-                else if (this.ActionType == TwitchActionType.EnableEmoteOnly)
-                {
-                    await ServiceManager.Get<ChatService>().SendMessage("/emoteonly", sendAsStreamer: true, platform: StreamingPlatformTypeEnum.Twitch);
-                }
-                else if (this.ActionType == TwitchActionType.DisableEmoteOnly)
-                {
-                    await ServiceManager.Get<ChatService>().SendMessage("/emoteonlyoff", sendAsStreamer: true, platform: StreamingPlatformTypeEnum.Twitch);
-                }
-                else if (this.ActionType == TwitchActionType.DisableFollowersOnly)
-                {
-                    await ServiceManager.Get<ChatService>().SendMessage("/followersoff", sendAsStreamer: true, platform: StreamingPlatformTypeEnum.Twitch);
-                }
-                else if (this.ActionType == TwitchActionType.DisableSlowChat)
-                {
-                    await ServiceManager.Get<ChatService>().SendMessage("/slowoff", sendAsStreamer: true, platform: StreamingPlatformTypeEnum.Twitch);
-                }
-                else if (this.ActionType == TwitchActionType.EnableSubscribersChat)
-                {
-                    await ServiceManager.Get<ChatService>().SendMessage("/subscribers", sendAsStreamer: true, platform: StreamingPlatformTypeEnum.Twitch);
-                }
-                else if (this.ActionType == TwitchActionType.DisableSubscriberChat)
-                {
-                    await ServiceManager.Get<ChatService>().SendMessage("/subscribersoff", sendAsStreamer: true, platform: StreamingPlatformTypeEnum.Twitch);
                 }
                 else if (this.ActionType == TwitchActionType.SetTitle)
                 {
