5816db66e7c9afd249a4486c8ad6e3bb2d5b08e6
Matthew Olivo <saviorxtanren@gmail.com>
2022-10-27 20:15:45 -0700

    - Progress check-in of widget editor

---
+++ MixItUp.WPF/Windows/Overlay/OverlayWidgetV3EditorWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MixItUp.WPF.Windows.Overlay
+{
+    /// <summary>
+    /// Interaction logic for OverlayWidgetV3EditorWindow.xaml
+    /// </summary>
+    public partial class OverlayWidgetV3EditorWindow : LoadingWindowBase
+    {
+        public OverlayWidgetV3EditorWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
