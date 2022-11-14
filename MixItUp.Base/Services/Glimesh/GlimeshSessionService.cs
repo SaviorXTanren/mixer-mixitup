9e4597250ae96604b504495bf8a75f3d987f3c4f
Matthew Olivo <saviorxtanren@gmail.com>
2022-11-14 00:49:23 -0800

    - Adding title updating for Glimesh platform

--- MixItUp.Base/Services/Glimesh/GlimeshSessionService.cs
+++ MixItUp.Base/Services/Glimesh/GlimeshSessionService.cs
@@ -299,7 +299,11 @@ namespace MixItUp.Base.Services.Glimesh
             return Task.FromResult(this.User?.channel?.title);
         }
 
-        public Task<bool> SetTitle(string title) { return Task.FromResult(false); }
+        public async Task<bool> SetTitle(string title)
+        {
+            await ServiceManager.Get<GlimeshSessionService>().UserConnection.UpdateStreamInfo(this.ChannelID, title);
+            return true;
+        }
 
         public Task<string> GetGame()
         {
