252c21864ec2a082c72dc8ffa60f8834268d73c7
Shane DeSeranno <tyren@syntheticstream.com>
2023-03-21 11:09:23 -0700

    Clean up the loupedeck plug-in

--- Loupedeck/MixItUpPlugin/MixItUpPlugin.cs
+++ Loupedeck/MixItUpPlugin/MixItUpPlugin.cs
@@ -2,27 +2,27 @@ using Loupedeck;
 
 namespace MixItUp.LoupedeckPlugin
 {
-    // This class contains the plugin-level logic of the Loupedeck plugin.
-
     public class MixItUpPlugin : Plugin
     {
+        // More info: https://github.com/Loupedeck/PluginSdk/wiki/Plugin-Capabilities
+
         // Gets a value indicating whether this is an Universal plugin or an Application plugin.
         public override bool UsesApplicationApiOnly => true;
 
         // Gets a value indicating whether this is an API-only plugin.
-        public override bool HasNoApplication => false;
+        public override bool HasNoApplication => true;
 
         // This method is called when the plugin is loaded during the Loupedeck service start-up.
-        public override void Load()
+        public override async void Load()
         {
-            //try
-            //{
-            //    var allCommands = await MixItUp.API.Commands.GetAllCommandsAsync();
-            //}
-            //catch
-            //{
-            //    this.OnPluginStatusChanged(Loupedeck.PluginStatus.Error, "Mix It Up is not running or developer APIs are not enabled.");
-            //}
+            try
+            {
+                var allCommands = await MixItUp.API.Commands.GetAllCommandsAsync();
+            }
+            catch
+            {
+                this.OnPluginStatusChanged(Loupedeck.PluginStatus.Error, "Mix It Up is not running or developer APIs are not enabled.");
+            }
         }
 
         // This method is called when the plugin is unloaded during the Loupedeck service shutdown.
