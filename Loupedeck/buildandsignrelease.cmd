cb720aaeacd511fee71201aa7cc6f3418003bb4c
Shane DeSeranno <tyren@syntheticstream.com>
2023-03-24 16:22:39 -0700

    Fix up issues with loupedeck plugin

---
+++ Loupedeck/buildandsignrelease.cmd
@@ -0,0 +1,8 @@
+msbuild /t:Clean,Build /property:Configuration=Release MixItUpPlugin\MixItUp.LoupedeckPlugin.csproj
+
+signtool.exe sign /fd sha256 /n "Blazing Cacti LLC" /tr http://ts.ssl.com /td sha256 /v ".\bin\Release\win\MixItUp.API.dll" ".\bin\Release\win\MixItUp.LoupedeckPlugin.dll"
+signtool.exe verify /pa ".\bin\Release\win\MixItUp.API.dll" ".\bin\Release\win\MixItUp.LoupedeckPlugin.dll"
+
+del ".\MixItUp_Loupedeck.zip"
+powershell -NoLogo -NoProfile -Command Compress-Archive -Path ".\bin\Release\*" -DestinationPath ".\MixItUp_Loupedeck.zip"
+move ".\MixItUp_Loupedeck.zip" ".\MixItUp_Loupedeck.lplug4"
