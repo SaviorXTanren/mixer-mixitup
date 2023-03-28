43a83de14e111af0454ab8c2dfef53f65b7bd106
Matthew Olivo <saviorxtanren@gmail.com>
2023-03-27 21:16:02 -0700

    - Adjusting moderation action to perform platform search for user

--- MixItUp.Base/Model/Actions/ModerationActionModel.cs
+++ MixItUp.Base/Model/Actions/ModerationActionModel.cs
@@ -61,7 +61,7 @@ namespace MixItUp.Base.Model.Actions
                 if (!string.IsNullOrEmpty(this.TargetUsername))
                 {
                     string username = await ReplaceStringWithSpecialModifiers(this.TargetUsername, parameters);
-                    targetUser = ServiceManager.Get<UserService>().GetActiveUserByPlatformUsername(parameters.Platform, username);
+                    targetUser = await ServiceManager.Get<UserService>().GetUserByPlatformUsername(parameters.Platform, username, performPlatformSearch: true);
                 }
                 else
                 {
