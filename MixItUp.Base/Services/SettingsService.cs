2ec951dce2dd3561e368aa310d015f1d0b5e40e3
Matthew Olivo <saviorxtanren@gmail.com>
2023-07-07 19:33:04 -0700

    - Initial version 7 settings upgrade

--- MixItUp.Base/Services/SettingsService.cs
+++ MixItUp.Base/Services/SettingsService.cs
@@ -363,6 +363,7 @@ namespace MixItUp.Base.Services
             else if (currentVersion < SettingsV3Model.LatestVersion)
             {
                 await SettingsV3Upgrader.Version6Upgrade(currentVersion, filePath);
+                await SettingsV3Upgrader.Version7Upgrade(currentVersion, filePath);
             }
             SettingsV3Model settings = await FileSerializerHelper.DeserializeFromFile<SettingsV3Model>(filePath, ignoreErrors: true);
             settings.Version = SettingsV3Model.LatestVersion;
@@ -382,6 +383,20 @@ namespace MixItUp.Base.Services
                     // Force OAuth token reset for new scopes
                     settings.StreamingPlatformAuthentications[StreamingPlatformTypeEnum.Twitch].UserOAuthToken.Reset();
                 }
+
+                await ServiceManager.Get<SettingsService>().Save(settings);
+            }
+        }
+
+        public static async Task Version7Upgrade(int version, string filePath)
+        {
+            if (version < 7)
+            {
+                string fileData = await ServiceManager.Get<IFileService>().ReadFile(filePath);
+                SettingsV3Model settings = await FileSerializerHelper.DeserializeFromFile<SettingsV3Model>(filePath, ignoreErrors: true);
+                await settings.Initialize();
+
+
 
                 await ServiceManager.Get<SettingsService>().Save(settings);
             }
