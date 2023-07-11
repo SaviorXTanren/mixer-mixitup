b6e706c58c8381071a241d5eed2bab0507c5e585
Matthew Olivo <saviorxtanren@gmail.com>
2023-07-11 00:36:16 -0700

    - Updating Trovo authentication to use authorization code flow to handle automatic token refreshing

--- MixItUp.Base/Services/Trovo/TrovoPlatformService.cs
+++ MixItUp.Base/Services/Trovo/TrovoPlatformService.cs
@@ -101,7 +101,8 @@ namespace MixItUp.Base.Services.Trovo
         {
             try
             {
-                TrovoConnection connection = await TrovoConnection.ConnectViaLocalhostOAuthBrowser(TrovoPlatformService.ClientID, scopes, forceApprovalPrompt: true, successResponse: OAuthExternalServiceBase.LoginRedirectPageHTML);
+                TrovoConnection connection = await TrovoConnection.ConnectViaLocalhostOAuthBrowser(TrovoPlatformService.ClientID, ServiceManager.Get<SecretsService>().GetSecret("TrovoSecret"),
+                    scopes, forceApprovalPrompt: true, successResponse: OAuthExternalServiceBase.LoginRedirectPageHTML);
                 if (connection != null)
                 {
                     return new Result<TrovoPlatformService>(new TrovoPlatformService(connection));
