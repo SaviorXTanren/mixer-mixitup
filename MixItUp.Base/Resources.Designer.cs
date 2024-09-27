9e59eff972a1e955d9440e955636090c728e2c92
Matthew Olivo <saviorxtanren@outlook.com>
2024-09-26 23:03:10 -0700

    - Updating resources

--- MixItUp.Base/Resources.Designer.cs
+++ MixItUp.Base/Resources.Designer.cs
@@ -24624,7 +24624,7 @@ namespace MixItUp.Base {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Unable to display clip due to newer format by Twitch. Please up-vote the following User Voice to encourage Twitch to allow this functionality for: https://twitch.uservoice.com/forums/310213-developers/suggestions/39228784-extend-clips-api-to-provide-the-mp4-url-so-editors.
+        ///   Looks up a localized string similar to Unable to display clip due to newer format by Twitch. Please up-vote the following User Voice to encourage Twitch to allow for this functionality: https://twitch.uservoice.com/forums/310213-developers/suggestions/39228784-extend-clips-api-to-provide-the-mp4-url-so-editors.
         /// </summary>
         public static string TwitchClipNewerClipFormatUnsupported {
             get {
