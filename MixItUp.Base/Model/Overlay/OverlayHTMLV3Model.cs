20d271b4090743c054bbe94b7e9831cb035438ec
Matthew Olivo <saviorxtanren@outlook.com>
2024-10-01 23:50:16 -0700

    - Fixing bug with HTML Overlay Widget Javascript not properly refreshing the entire HTML contents

--- MixItUp.Base/Model/Overlay/OverlayHTMLV3Model.cs
+++ MixItUp.Base/Model/Overlay/OverlayHTMLV3Model.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.Serialization;
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
 
 namespace MixItUp.Base.Model.Overlay
 {
@@ -10,5 +12,21 @@ namespace MixItUp.Base.Model.Overlay
         public static readonly string DefaultJavascript = OverlayResources.OverlayActionDefaultJavascript;
 
         public OverlayHTMLV3Model() : base(OverlayItemV3Type.HTML) { }
+
+        public override Task Initialize()
+        {
+            if (string.Equals(this.Javascript, OverlayResources.OverlayHTMLWidgetDefaultJavascriptOld, System.StringComparison.OrdinalIgnoreCase))
+            {
+                this.Javascript = OverlayResources.OverlayHTMLWidgetDefaultJavascript;
+            }
+            return base.Initialize();
+        }
+
+        public override Dictionary<string, object> GetGenerationProperties()
+        {
+            Dictionary<string, object> properties = base.GetGenerationProperties();
+            properties[nameof(this.HTML)] = this.HTML;
+            return properties;
+        }
     }
 }
