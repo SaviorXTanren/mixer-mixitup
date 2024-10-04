56a7cc7a60d35b906175956fe5799d19b0eb9771
Matthew Olivo <saviorxtanren@outlook.com>
2024-10-03 20:59:21 -0700

    System.NullReferenceException: Object reference not set to an instance of an object.    at MixItUp.WPF.Services.OBSWebsocketV5.<HandleHello>d__39.MoveNext() in S:\Code\mixer-mixitup\MixItUp.WPF\Services\WindowsOBSService.cs:line 968

--- MixItUp.WPF/Services/WindowsOBSService.cs
+++ MixItUp.WPF/Services/WindowsOBSService.cs
@@ -965,7 +965,7 @@ namespace MixItUp.WPF.Services
         private async Task HandleHello(OBSMessageHello message)
         {
             OBSMessageIdentify identify = new OBSMessageIdentify();
-            if (message.Data.Authentication != null)
+            if (message != null && message.Data != null && message.Data.Authentication != null)
             {
                 // To generate the authentication string, follow these steps:
                 // Concatenate the websocket password with the salt provided by the server(password + salt)
