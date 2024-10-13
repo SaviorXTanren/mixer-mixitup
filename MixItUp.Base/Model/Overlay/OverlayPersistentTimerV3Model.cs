f64d9fea95857d0e179eeeac6f41f1cb553a0771
Matthew Olivo <saviorxtanren@outlook.com>
2024-10-12 18:32:58 -0700

    - Adding assurance that previous Persistent Timer Overlay Widget countdowns are canceled when a new one starts up

--- MixItUp.Base/Model/Overlay/OverlayPersistentTimerV3Model.cs
+++ MixItUp.Base/Model/Overlay/OverlayPersistentTimerV3Model.cs
@@ -162,6 +162,10 @@ namespace MixItUp.Base.Model.Overlay
 
         private void StartBackgroundTimer()
         {
+            if (this.cancellationTokenSource != null)
+            {
+                this.cancellationTokenSource.Cancel();
+            }
             this.cancellationTokenSource = new CancellationTokenSource();
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             AsyncRunner.RunAsyncBackground(this.BackgroundTimer, this.cancellationTokenSource.Token);
